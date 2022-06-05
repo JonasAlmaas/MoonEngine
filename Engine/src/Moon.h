4af0cc27f95b0a7f154f25960f5be84a6f0050ae
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-05 20:13:38 +0200

    Remove Entry point from Moon.h

--- Engine/src/Moon.h
+++ Engine/src/Moon.h
@@ -3,7 +3,3 @@
 // For use by Moon engine applications only
 
 #include "Moon/Core/Application/Application.h"
-
-// --- Entry Point --------------------------
-#include "Moon/Core/Application/Entrypoint.h"
-// ------------------------------------------
