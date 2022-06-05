df7cbdc59094a1c9c82664d7926b46bcb41e3df4
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-05 21:53:17 +0200

    Create premake for project

---
+++ premake5.lua
@@ -0,0 +1,116 @@
+
+workspace "MoonEngine"
+    architecture "x86_64"
+    startproject "Sandbox"
+
+    configurations  {
+		"Debug",
+		"Release",
+		"Dist",
+	}
+
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+
+project "Engine"
+    location "Engine"
+    kind "SharedLib"
+    language "C++"
+    cppdialect "C++20"
+    staticruntime "On"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
+    }
+
+    includedirs
+    {
+        "%{prj.name}/src",
+        "%{prj.name}/vendor/spdlog/include",
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+        defines
+        {
+            "ME_PLATFORM_WINDOWS",
+            "ME_BUILD_DLL",
+        }
+
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+
+    filter "configurations:Debug"
+		defines "ME_DEBUG"
+		runtime "Debug"
+		symbols "on"
+	
+	filter "configurations:Release"
+		defines "ME_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "ME_DIST"
+		runtime "Release"
+		optimize "on"
+
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+	language "C++"
+    cppdialect "C++20"
+	staticruntime "on"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp",
+    }
+    
+    includedirs
+    {
+        "Engine/src",
+        "Engine/vendor/spdlog/include",
+    }
+
+    links
+    {
+        "Engine"
+    }
+
+	filter "system:windows"
+		systemversion "latest"
+
+        defines
+        {
+            "ME_PLATFORM_WINDOWS",
+        }
+
+	filter "configurations:Debug"
+		defines "FN_DEBUG"
+		runtime "Debug"
+		symbols "on"
+	
+	filter "configurations:Release"
+		defines "FN_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "FN_DIST"
+		runtime "Release"
+		optimize "on"
