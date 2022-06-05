d698d7f971675280c92b6873eb038fc6d7b26a2e
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-05 20:10:20 +0200

    Setup basic entrypoint

--- Engine/src/Moon/Core/Application/Entrypoint.h
+++ Engine/src/Moon/Core/Application/Entrypoint.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "Moon/Core/Base.h"
+#include "Moon/Core/Application/Application.h"
+
+
+#ifdef ME_PLATFORM_WINDOWS
+	
+	extern Moon::Application* Moon::CreateApplication();
+	
+	int main(int argc, char** argv)
+	{
+		auto app = Moon::CreateApplication();
+		app->Run();
+		delete app;
+	}
+
+#endif
