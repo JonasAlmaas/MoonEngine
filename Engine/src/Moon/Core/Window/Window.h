7565d87bfbe372385bf77ab5fa8939187daba8b4
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-05 23:31:54 +0200

    Basic window interface

--- Engine/src/Moon/Core/Window/Window.h
+++ Engine/src/Moon/Core/Window/Window.h
@@ -1,1 +1,46 @@
 #pragma once
+
+#include "mepch.h"
+
+#include "Moon/Core/Base.h"
+#include "Moon/Events/Event.h"
+
+
+namespace Moon {
+
+	struct WindowProps
+	{
+		std::string Title;
+		unsigned int Width;
+		unsigned int Height;
+
+		WindowProps(const std::string& title = "Moon Engine",
+					unsigned int width = 1280,
+					unsigned int height = 720)
+			: Title(title), Width(width), Height(height)
+		{
+		}
+	};
+
+	// Interface representing a desktop system based window
+	class MOON_API Window
+	{
+	public:
+		using EventCallbackFn = std::function<void(Event&)>;
+
+		virtual ~Window() {}
+
+		virtual void OnUpdate() = 0;
+
+		virtual unsigned int GetWidth() const = 0;
+		virtual unsigned int GetHeight() const = 0;
+
+		// Window attributes
+		virtual void SetEventCallback(const EventCallbackFn& callback) = 0;
+		virtual void SetVsync(bool enabled) = 0;
+		virtual bool IsVsync() const = 0;
+
+		static Window* Create(const WindowProps& props = WindowProps());
+	};
+
+}
