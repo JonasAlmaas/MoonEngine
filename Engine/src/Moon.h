9c4bef71ec5d21387d245aa0e54f549ddd454b55
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-05 20:10:59 +0200

    Add Entrypoint to Moon.h

--- Engine/src/Moon.h
+++ Engine/src/Moon.h
@@ -3,3 +3,7 @@
 // For use by Moon engine applications only
 
 #include "Moon/Core/Application/Application.h"
+
+// --- Entry Point --------------------------
+#include "Moon/Core/Application/Entrypoint.h"
+// ------------------------------------------
