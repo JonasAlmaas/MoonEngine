b82f5d1c828ae0071e158ebb52e165782df87235
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-05 20:06:57 +0200

    Add dll import/export macors

--- Engine/src/Moon/Core/Base.h
+++ Engine/src/Moon/Core/Base.h
@@ -1,1 +1,12 @@
 #pragma once
+
+
+#ifdef ME_PLATFORM_WINDOWS
+	#ifdef ME_BUILD_DLL
+		#define MOON_API __declspec(dllexport)
+	#else
+		#define MOON_API __declspec(dllimport)
+	#endif
+#else
+	#error MoonEngine only supports Windows!
+#endif
