4e5f816452c0e5ab08d856f9a25b4ec780b16eda
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-06 16:23:24 +0200

    Create Layer class

---
+++ Engine/src/Moon/Core/Layer/Layer.cpp
@@ -0,0 +1,16 @@
+#include "mepch.h"
+#include "Moon/Core/Layer/Layer.h"
+
+
+namespace Moon {
+
+	Layer::Layer(const std::string& name)
+		: m_DebugName(name)
+	{
+	}
+
+	Layer::~Layer()
+	{
+	}
+
+}
