c5e22393ba31def0cd1bed74e2aae047464eb752
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-06 20:51:53 +0200

    Create windows input

---
+++ Engine/src/Platform/Windows/Input/WindowsInput.cpp
@@ -0,0 +1,47 @@
+#include "mepch.h"
+#include "Platform/Windows/Input/WindowsInput.h"
+
+#include "Moon/Core/Application/Application.h"
+
+#include <GLFW/glfw3.h>
+
+
+namespace Moon {
+
+	Input* Input::s_Instance = new WindowsInput();
+
+	bool WindowsInput::IsKeyPressedImpl(int keycode)
+	{
+		GLFWwindow* window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		int state = glfwGetKey(window, keycode);
+		return state == GLFW_PRESS || state == GLFW_REPEAT;
+	}
+
+	bool WindowsInput::IsMouseButtonPressedImpl(int button)
+	{
+		GLFWwindow* window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		int state = glfwGetMouseButton(window, button);
+		return state == GLFW_PRESS;
+	}
+
+	std::pair<float, float> WindowsInput::GetMousePositionImpl()
+	{
+		GLFWwindow* window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		double x, y;
+		glfwGetCursorPos(window, &x, &y);
+		return { (float)x, (float)y};
+	}
+
+	float WindowsInput::GetMouseXImpl()
+	{
+		auto[x, y] = GetMousePositionImpl();
+		return x;
+	}
+
+	float WindowsInput::GetMouseYImpl()
+	{
+		auto[x, y] = GetMousePositionImpl();
+		return y;
+	}
+
+}
