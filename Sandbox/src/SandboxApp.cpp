00f2580c45d022fa70df303a7e1774d0804e7588
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-06 16:24:22 +0200

    Update Sandbox with example layer

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -2,12 +2,31 @@
 #include <Moon/Core/Application/Entrypoint.h>
 
 
+class ExampleLayer : public Moon::Layer
+{
+public:
+	ExampleLayer()
+		: Layer("Example")
+	{
+	}
+
+	void OnUpdate() override
+	{
+
+	}
+
+	void OnEvent(Moon::Event& e) override
+	{
+		ME_TRACE("{0}", e);
+	}
+};
+
 class Sandbox : public Moon::Application
 {
 public:
 	Sandbox()
 	{
-
+		PushLayer(new ExampleLayer());
 	}
 
 	~Sandbox()
