138a6587c045ff9427a77582bda814dd76eb0d7b
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-06 20:51:39 +0200

    Add virtual before virtual functions
    This is not needed, but looks clean

--- Engine/src/Platform/Windows/Window/WindowsWindow.h
+++ Engine/src/Platform/Windows/Window/WindowsWindow.h
@@ -13,15 +13,15 @@ namespace Moon {
 		WindowsWindow(const WindowProps& props);
 		virtual ~WindowsWindow();
 
-		void OnUpdate() override;
+		virtual void OnUpdate() override;
 
-		inline unsigned int GetWidth() const override { return m_Data.Width; };
-		inline unsigned int GetHeight() const override { return m_Data.Height; };
+		virtual inline unsigned int GetWidth() const override { return m_Data.Width; };
+		virtual inline unsigned int GetHeight() const override { return m_Data.Height; };
 
 		// Window attributes
-		inline void SetEventCallback(const EventCallbackFn& callback) override { m_Data.EventCallback = callback; }
-		void SetVSync(bool enabled) override;
-		bool IsVSync() const override;
+		virtual inline void SetEventCallback(const EventCallbackFn& callback) override { m_Data.EventCallback = callback; }
+		virtual void SetVSync(bool enabled) override;
+		virtual bool IsVSync() const override;
 
 		virtual inline void* GetNativeWindow() const override { return m_Window; };
 
