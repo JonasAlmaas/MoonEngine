0e1003483767a82718186bd5fc1ad00d41207ec1
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-06 23:17:14 +0200

    Add OnImGuiRender to Layer

--- Engine/src/Moon/Core/Layer/Layer.h
+++ Engine/src/Moon/Core/Layer/Layer.h
@@ -15,6 +15,7 @@ namespace Moon {
 		virtual void OnAttach() {}
 		virtual void OnDetach() {}
 		virtual void OnUpdate() {}
+		virtual void OnImGuiRender() {}
 		virtual void OnEvent(Event& e) {}
 
 		inline const std::string& GetName() const { return m_DebugName; }
