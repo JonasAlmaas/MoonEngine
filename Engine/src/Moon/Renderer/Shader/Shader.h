13ac1137902a29041bed5d0ff847bdf044a0abc3
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-08 17:58:39 +0200

    Create basic shader compilation

---
+++ Engine/src/Moon/Renderer/Shader/Shader.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+
+
+namespace Moon {
+
+	class Shader
+	{
+	public:
+		Shader(const std::string& vertexSrc, const std::string& fragmentSrc);
+		~Shader();
+
+		void Bind() const;
+		void UnBind() const;
+
+	private:
+		uint32_t m_RendererID;
+
+	};
+
+}
