05b3ee959cd4edfc9b5d40fbbdad107f124fa1d4
JonasAlmaas <44512227+JonasAlmaas@users.noreply.github.com>
2022-06-08 22:14:44 +0200

    Update VertexBufferUtils.h

--- Engine/src/Moon/Core/Renderer/VertexBuffer/VertexBufferUtils.h
+++ Engine/src/Moon/Core/Renderer/VertexBuffer/VertexBufferUtils.h
@@ -22,9 +22,9 @@ namespace Moon {
 			case ShaderDataType::Int3:		return 4 * 3;
 			case ShaderDataType::Int4:		return 4 * 4;
 			case ShaderDataType::Float:		return 4;
-			case ShaderDataType::Float2:	return 4 * 2;
-			case ShaderDataType::Float3:	return 4 * 3;
-			case ShaderDataType::Float4:	return 4 * 4;
+			case ShaderDataType::Float2:		return 4 * 2;
+			case ShaderDataType::Float3:		return 4 * 3;
+			case ShaderDataType::Float4:		return 4 * 4;
 			case ShaderDataType::Mat3:		return 4 * 3 * 3;
 			case ShaderDataType::Mat4:		return 4 * 4 * 4;
 		}
