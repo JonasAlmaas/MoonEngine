79018dd51a1a5e1ad7bc3da0f45076fdb9079d46
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-08 19:22:55 +0200

    Create OpenGL index buffer

---
+++ Engine/src/Platform/OpenGL/Buffer/IndexBuffer/OpenGLIndexBuffer.cpp
@@ -0,0 +1,33 @@
+#include "mepch.h"
+#include "Platform/OpenGL/Buffer/IndexBuffer/OpenGLIndexBuffer.h"
+
+#include <glad/glad.h>
+
+
+namespace Moon {
+
+	OpenGLIndexBuffer::OpenGLIndexBuffer(uint32_t* indices, uint32_t count)
+		: m_Count(count)
+	{
+		glCreateBuffers(1, &m_RendererID);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(uint32_t), indices, GL_STATIC_DRAW);
+	}
+
+	OpenGLIndexBuffer::~OpenGLIndexBuffer()
+	{
+		glDeleteBuffers(1, &m_RendererID);
+	}
+
+
+	void OpenGLIndexBuffer::Bind() const
+	{
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_RendererID);
+	}
+
+	void OpenGLIndexBuffer::UnBind() const
+	{
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	}
+
+}
---
+++ Engine/src/Platform/OpenGL/Buffer/IndexBuffer/OpenGLIndexBuffer.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Moon/Core/Renderer/Buffer/IndexBuffer/IndexBuffer.h"
+
+
+namespace Moon {
+
+	class OpenGLIndexBuffer : public IndexBuffer
+	{
+	public:
+		OpenGLIndexBuffer(uint32_t* indices, uint32_t count);
+		virtual ~OpenGLIndexBuffer();
+
+		virtual void Bind() const override;
+		virtual void UnBind() const override;
+
+		virtual uint32_t GetCount() const override { return m_Count; };
+
+	private:
+		uint32_t m_RendererID;
+		uint32_t m_Count;
+	};
+
+}
+
