564268ec1420e3d1c73c6a4dc3a4a8d432e58c55
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-08 22:14:12 +0200

    Create OpenGLVertexArray

---
+++ Engine/src/Moon/Platform/OpenGL/VertexArray/OpenGLVertexArray.cpp
@@ -0,0 +1,78 @@
+#include "mepch.h"
+#include "Moon/Platform/OpenGL/VertexArray/OpenGLVertexArray.h"
+
+#include <glad/glad.h>
+
+
+namespace Moon {
+
+	// TEMPORARY (maybe)
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+			case Moon::ShaderDataType::Bool:		return GL_BOOL;
+			case Moon::ShaderDataType::Int:			return GL_INT;
+			case Moon::ShaderDataType::Int2:		return GL_INT;
+			case Moon::ShaderDataType::Int3:		return GL_INT;
+			case Moon::ShaderDataType::Int4:		return GL_INT;
+			case Moon::ShaderDataType::Float:		return GL_FLOAT;
+			case Moon::ShaderDataType::Float2:		return GL_FLOAT;
+			case Moon::ShaderDataType::Float3:		return GL_FLOAT;
+			case Moon::ShaderDataType::Float4:		return GL_FLOAT;
+			case Moon::ShaderDataType::Mat3:		return GL_FLOAT;
+			case Moon::ShaderDataType::Mat4:		return GL_FLOAT;
+		}
+
+		ME_CORE_ASSERT(false, "Unknows ShaderDataType!");
+		return 0;
+	}
+
+	OpenGLVertexArray::OpenGLVertexArray()
+	{
+		glCreateVertexArrays(1, &m_RendererID);
+	}
+
+	OpenGLVertexArray::~OpenGLVertexArray()
+	{
+		glDeleteVertexArrays(1, &m_RendererID);
+	}
+
+	void OpenGLVertexArray::Bind() const
+	{
+		glBindVertexArray(m_RendererID);
+	}
+
+	void OpenGLVertexArray::UnBind() const
+	{
+		glBindVertexArray(0);
+	}
+
+	void OpenGLVertexArray::AddVertexBuffer(const std::shared_ptr<VertexBuffer>& vertexBuffer)
+	{
+		ME_CORE_ASSERT(vertexBuffer->GetLayout().GetElements().size(), "Vertex buffer has no layout!");
+
+		glBindVertexArray(m_RendererID);
+		vertexBuffer->Bind();
+
+		uint32_t index = 0;
+		const auto& layout = vertexBuffer->GetLayout();
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index, element.GetComponentCount(), ShaderDataTypeToOpenGLBaseType(element.Type), element.Normalized ? GL_TRUE : GL_FALSE, layout.GetStride(), (const void*)element.Offset);
+			index++;
+		}
+
+		m_VertexBuffers.push_back(vertexBuffer);
+	}
+
+	void OpenGLVertexArray::SetIndexBuffer(const std::shared_ptr<IndexBuffer>& indexBuffer)
+	{
+		glBindVertexArray(m_RendererID);
+		indexBuffer->Bind();
+
+		m_IndexBuffer = indexBuffer;
+	}
+
+}
