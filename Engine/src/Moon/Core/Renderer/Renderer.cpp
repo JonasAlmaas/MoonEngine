1f471bf58799b20ba56540b7181a6f965492ba13
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-08 19:22:09 +0200

    Create Renderer with hard coded API

---
+++ Engine/src/Moon/Core/Renderer/Renderer.cpp
@@ -0,0 +1,10 @@
+#include "mepch.h"
+#include "Moon/Core/Renderer/Renderer.h"
+
+
+namespace Moon {
+	
+	// TODO: Should be set at runtime
+	RendererAPI Renderer::s_RendererAPI = RendererAPI::OpenGL;
+
+}
