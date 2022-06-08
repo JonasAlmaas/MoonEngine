c967bf7b81ec33411d1e971e3fc9a94e03373678
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-08 17:18:49 +0200

    Add OpenGL info

--- Engine/src/Platform/OpenGL/GraphicsContext/OpenGLContext.cpp
+++ Engine/src/Platform/OpenGL/GraphicsContext/OpenGLContext.cpp
@@ -20,6 +20,11 @@ namespace Moon {
 		// Initialize Glad
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		ME_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		ME_CORE_INFO("OpenGL Info:");
+		ME_CORE_INFO("  Vendor: {0}", glGetString(GL_VENDOR));
+		ME_CORE_INFO("  Renderer: {0}", glGetString(GL_RENDERER));
+		ME_CORE_INFO("  Version: {0}", glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
