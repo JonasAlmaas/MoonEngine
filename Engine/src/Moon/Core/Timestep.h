5bde6459270b8179fe0f522af7b623a55d1cb42b
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-10 19:57:54 +0200

    Made Timestep operator float const

--- Engine/src/Moon/Core/Timestep.h
+++ Engine/src/Moon/Core/Timestep.h
@@ -11,7 +11,7 @@ namespace Moon {
 		{
 		}
 
-		operator float() { return m_Time; }
+		operator float() const { return m_Time; }
 
 		float GetSeconds() const { return m_Time; }
 		float GetMilliseconds() const { return m_Time * 1000.0f; }
