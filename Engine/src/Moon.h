512994e8186222f17de6939a19260f9476071677
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-10 16:08:39 +0200

    Add UIColor to Moon.h

--- Engine/src/Moon.h
+++ Engine/src/Moon.h
@@ -6,6 +6,7 @@
 #include "Moon/Core/Application/Application.h"
 
 #include "Moon/Core/Customization/Color.h"
+#include "Moon/Core/Customization/UIColor.h"
 
 #include "Moon/Core/Input/Input.h"
 #include "Moon/Core/Input/Code/KeyCode.h"
