8db10f7f86e53e072b581eb45d6265b86002ebdd
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-10 18:12:02 +0200

    Add UploadUniformMat4 to Shader

--- Engine/src/Moon/Core/Renderer/Shader/Shader.cpp
+++ Engine/src/Moon/Core/Renderer/Shader/Shader.cpp
@@ -3,6 +3,8 @@
 
 #include <glad/glad.h>
 
+#include <glm/gtc/type_ptr.hpp>
+
 
 namespace Moon {
 
@@ -122,6 +124,12 @@ namespace Moon {
 	void Shader::UnBind() const
 	{
 		glUseProgram(0);
+	}
+
+	void Shader::UploadUniformMat4(const std::string& name, const glm::mat4& matrix)
+	{
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(matrix));
 	}
 
 }
--- Engine/src/Moon/Core/Renderer/Shader/Shader.h
+++ Engine/src/Moon/Core/Renderer/Shader/Shader.h
@@ -2,6 +2,8 @@
 
 #include <string>
 
+#include <glm/glm.hpp>
+
 
 namespace Moon {
 
@@ -13,6 +15,15 @@ namespace Moon {
 
 		void Bind() const;
 		void UnBind() const;
+
+		/*
+		 * Uploads a 4 * 4 matrix to the gpu.
+		 * Binding the shader has to be done before uploading to it!
+		 * 
+		 * @param name of the uniform.
+		 * @param matrix to upload.
+		 */
+		void UploadUniformMat4(const std::string& name, const glm::mat4& matrix);
 
 	private:
 		uint32_t m_RendererID;
