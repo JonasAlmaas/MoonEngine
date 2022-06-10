126387ee1a8089580a650639417ff5d515930e0c
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-10 18:28:37 +0200

    Update Moon.h

--- Engine/src/Moon.h
+++ Engine/src/Moon.h
@@ -2,23 +2,32 @@
 
 // For use by Moon engine applications only
 
-// ---- Core ----
+// ---- Application ----
 #include "Moon/Core/Application/Application.h"
 
+// ---- Customization ----
 #include "Moon/Core/Customization/Color.h"
 #include "Moon/Core/Customization/UIColor.h"
 
+// ---- Input ----
 #include "Moon/Core/Input/Input.h"
 #include "Moon/Core/Input/Code/KeyCode.h"
 #include "Moon/Core/Input/Code/MouseCode.h"
 
+// ---- Layer ----
 #include "Moon/Core/Layer/Layer.h"
 
+// ---- Log ----
 #include "Moon/Core/Log/Log.h"
 
-#include "Moon/Core/Renderer/Camera/Orthographic/OrthographicCamera.h"
-#include "Moon/Core/Renderer/RenderCommand/RenderCommand.h"
+// ---- Renderer ----
 #include "Moon/Core/Renderer/Renderer.h"
+#include "Moon/Core/Renderer/Camera/Orthographic/OrthographicCamera.h"
+#include "Moon/Core/Renderer/IndexBuffer/IndexBuffer.h"
+#include "Moon/Core/Renderer/RenderCommand/RenderCommand.h"
 #include "Moon/Core/Renderer/Shader/Shader.h"
+#include "Moon/Core/Renderer/VertexArray/VertexArray.h"
+#include "Moon/Core/Renderer/VertexBuffer/VertexBuffer.h"
 
+// ---- ImGui ----
 #include "Moon/ImGui/ImGuiLayer.h"
