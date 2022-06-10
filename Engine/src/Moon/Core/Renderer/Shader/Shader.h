8f37664b15423d528b5c314129d8c226c59f79b3
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-11 00:28:06 +0200

    Move OpenGL code into Platfrom/OpenGL

--- Engine/src/Moon/Core/Renderer/Shader/Shader.h
+++ Engine/src/Moon/Core/Renderer/Shader/Shader.h
@@ -1,24 +1,17 @@
 #pragma once
 
-#include <glm/glm.hpp>
-
 
 namespace Moon {
 
 	class Shader
 	{
 	public:
-		Shader(const std::string& vertexSrc, const std::string& fragmentSrc);
-		~Shader();
+		virtual ~Shader() = default;
 
-		void Bind() const;
-		void UnBind() const;
+		virtual void Bind() const = 0;
+		virtual void UnBind() const = 0;
 
-		void UploadUniformFloat4(const std::string& name, const glm::vec4& float4);
-		void UploadUniformMat4(const std::string& name, const glm::mat4& matrix);
-
-	private:
-		uint32_t m_RendererID;
+		static Shader* Create(const std::string& vertexSrc, const std::string& fragmentSrc);
 
 	};
 
