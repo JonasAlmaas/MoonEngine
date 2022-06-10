6b91e770292a8c76f29c58e92d04eb572f3e9a33
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-10 21:00:53 +0200

    Update Shader to support transform

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -80,6 +80,7 @@ namespace Moon {
 				layout(location = 1) in vec4 a_Color;
 
 				uniform mat4 u_ViewProjection;
+				uniform mat4 u_Transform;
 
 				out vec3 v_Position;
 				out vec4 v_Color;
@@ -89,7 +90,7 @@ namespace Moon {
 					v_Position = a_Position;
 					v_Color = a_Color;
 
-					gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+					gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 				}
 			)";
 
