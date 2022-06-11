6d6610976fe5aad1b9cb9606702c9f38000629b2
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-11 02:09:41 +0200

    Create OpenGLTexture and OpenGLTexture2D

---
+++ Engine/src/Moon/Platform/OpenGL/Texture/OpenGLTexture.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Moon/Core/Renderer/Texture/Texture.h"
+
+#include <glad/glad.h>
+
+
+namespace Moon {
+
+	class OpenGLTexture2D : public Texture2D
+	{
+	public:
+		OpenGLTexture2D(const std::string& filepath);
+		OpenGLTexture2D(uint32_t width, uint32_t height);
+
+		virtual ~OpenGLTexture2D();
+
+		virtual uint32_t GetWidth() const override { return m_Width; }
+		virtual uint32_t GetHeight() const override { return m_Height; }
+		virtual uint32_t GetRendererID() const override { return m_RendererID; }
+
+		virtual void Bind(uint32_t slot) const override;
+
+		virtual bool operator==(const Texture& other) const override { return m_RendererID == ((OpenGLTexture2D&)other).m_RendererID; }
+
+	private:
+		uint32_t m_RendererID;
+		std::string m_Path;
+		uint32_t m_Width, m_Height;
+		GLenum m_InternalFormat, m_DataFormat;
+
+	};
+
+}
