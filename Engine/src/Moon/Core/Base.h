0e79e612abade21746a7480da8931ec0bb5992e6
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-11 02:11:42 +0200

    Add expression for CreateScope and CreateRef

--- Engine/src/Moon/Core/Base.h
+++ Engine/src/Moon/Core/Base.h
@@ -2,7 +2,6 @@
 
 #include "Moon/Core/Assert.h"
 
-
 #ifdef ME_DEBUG
 	#if defined(ME_PLATFORM_WINDOWS)
 		#define ME_DEBUGBREAK() __debugbreak()
@@ -27,7 +26,19 @@ namespace Moon {
 	template<typename T>
 	using Scope = std::unique_ptr<T>;
 
+	template<typename T, typename ... Args>
+	constexpr Scope<T> CreateScope(Args&& ... args)
+	{
+		return std::make_unique<T>(std::forward<Args>(args)...);
+	}
+
 	template<typename T>
 	using Ref = std::shared_ptr<T>;
+
+	template<typename T, typename ... Args>
+	constexpr Ref<T> CreateRef(Args&& ... args)
+	{
+		return std::make_shared<T>(std::forward<Args>(args)...);
+	}
 
 }
