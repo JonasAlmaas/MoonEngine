1bb13f14d1be1d1828a16089553d03466975bb72
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-12 18:17:23 +0200

    Add Set and Get ZoomLevel to OrthographicCameraController

--- Engine/src/Moon/CameraController/OrthographicCameraController/OrthographicCameraController.h
+++ Engine/src/Moon/CameraController/OrthographicCameraController/OrthographicCameraController.h
@@ -19,6 +19,9 @@ namespace Moon {
 		inline OrthographicCamera& GetCamera() { return m_Camera; }
 		inline const OrthographicCamera& GetCamera() const { return m_Camera; }
 
+		float GetZoomLevel() const { return m_ZoomLevel; }
+		void SetZoomLevel(float level) { m_ZoomLevel = level; }
+
 	private:
 		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
 		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
