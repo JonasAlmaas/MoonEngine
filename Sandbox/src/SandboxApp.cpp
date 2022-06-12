fe8dfa4a72aa0cb04b881934ca70ca6d2f7a5e68
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-12 11:18:58 +0200

    Remove OpenSans from Sandbox

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -189,10 +189,6 @@ namespace Moon {
 	public:
 		Sandbox()
 		{
-			ImGuiIO& io = ImGui::GetIO();
-			io.Fonts->AddFontFromFileTTF("assets/fonts/opensans/OpenSans-Bold.ttf", 18.0f);
-			io.FontDefault = io.Fonts->AddFontFromFileTTF("assets/fonts/opensans/OpenSans-Regular.ttf", 18.0f);
-
 			PushLayer(CreateRef<ExampleLayer>());
 		}
 
