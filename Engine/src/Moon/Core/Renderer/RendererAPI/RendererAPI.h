ea6978c30196e055529ca23c998b69d8901e3e78
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-12 18:15:10 +0200

    Add SetViewport method to the renderer

--- Engine/src/Moon/Core/Renderer/RendererAPI/RendererAPI.h
+++ Engine/src/Moon/Core/Renderer/RendererAPI/RendererAPI.h
@@ -17,6 +17,7 @@ namespace Moon {
 
 	public:
 		virtual void Init() = 0;
+		virtual void SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height) = 0;
 
 		virtual void SetClearColor(const Color& color) = 0;
 		virtual void Clear() = 0;
--- Engine/src/Moon/Platform/OpenGL/RendererAPI/OpenGLRendererAPI.h
+++ Engine/src/Moon/Platform/OpenGL/RendererAPI/OpenGLRendererAPI.h
@@ -9,6 +9,7 @@ namespace Moon {
 	{
 	public:
 		virtual void Init() override;
+		virtual void SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height) override;
 
 		virtual void SetClearColor(const Color& color) override;
 		virtual void Clear() override;
