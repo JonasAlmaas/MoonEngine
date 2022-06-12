fdfea4f0aeaae3d9d514783207b7843b39d2caff
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-12 20:08:13 +0200

    Move ExampleLayer into its own files

---
+++ Sandbox/src/ExampleLayer/ExampleLayer.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <Moon.h>
+
+using namespace Moon;
+
+
+class ExampleLayer : public Layer
+{
+public:
+	ExampleLayer();
+
+	virtual void OnAttach() override;
+	virtual void OnDetach() override;
+	virtual void OnUpdate(Timestep ts) override;
+	virtual void OnImGuiRender() override;
+	virtual void OnEvent(Event& e) override;
+
+private:
+	OrthographicCameraController m_CameraController;
+
+	ShaderLibrary m_ShaderLibrary;
+
+	Ref<VertexArray> m_SquareVA;
+	Ref<VertexArray> m_TriangleVA;
+
+	Ref<Texture2D> m_Texture;
+	Ref<Texture2D> m_TransparentTexture;
+
+	glm::vec3 m_Color1 = { 0.8f, 0.2f, 0.3f };
+	glm::vec3 m_Color2 = { 0.3f, 0.2f, 0.8f };
+
+};
