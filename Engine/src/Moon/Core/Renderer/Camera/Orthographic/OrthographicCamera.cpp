c00bbb230c6c5f8faa9e319e28261514d306ffbd
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-13 16:16:39 +0200

    Use initializer list instead of glm::vec

--- Engine/src/Moon/Core/Renderer/Camera/Orthographic/OrthographicCamera.cpp
+++ Engine/src/Moon/Core/Renderer/Camera/Orthographic/OrthographicCamera.cpp
@@ -38,7 +38,7 @@ namespace Moon {
 
 	void OrthographicCamera::RecalculateViewMatrix()
 	{
-		glm::mat4 transform = glm::translate(glm::mat4(1.0f), m_Position) * glm::rotate(glm::mat4(1.0f), m_RotationRadians, glm::vec3(0, 0, 1));
+		glm::mat4 transform = glm::translate(glm::mat4(1.0f), m_Position) * glm::rotate(glm::mat4(1.0f), m_RotationRadians, { 0.0f, 0.0f, 1.0f });
 		m_ViewMatrix = glm::inverse(transform);
 
 		m_ViewProjectionMatrix = m_ProjectionMatrix * m_ViewMatrix;
