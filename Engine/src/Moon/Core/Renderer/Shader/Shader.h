e556af5873fa66fdc01c4c15445e16d9f4c1f229
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-13 18:39:53 +0200

    Add SetFloat2 to Shader

--- Engine/src/Moon/Core/Renderer/Shader/Shader.h
+++ Engine/src/Moon/Core/Renderer/Shader/Shader.h
@@ -18,6 +18,7 @@ namespace Moon {
 		virtual const std::string& GetName() const = 0;
 
 		virtual void SetInt(const std::string& name, int value) = 0;
+		virtual void SetFloat2(const std::string& name, const glm::vec2& value) = 0;
 		virtual void SetFloat3(const std::string& name, const glm::vec3& value) = 0;
 		virtual void SetFloat4(const std::string& name, const glm::vec4& value) = 0;
 		virtual void SetMat4(const std::string& name, const glm::mat4& matrix) = 0;
