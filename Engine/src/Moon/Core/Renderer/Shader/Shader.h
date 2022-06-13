15d78e42a175a21df6516c42f0de47031905af43
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-13 15:56:11 +0200

    Add setters for uniforms in Shader

--- Engine/src/Moon/Core/Renderer/Shader/Shader.h
+++ Engine/src/Moon/Core/Renderer/Shader/Shader.h
@@ -2,6 +2,8 @@
 
 #include "Moon/Core/Base.h"
 
+#include <glm/glm.hpp>
+
 
 namespace Moon {
 
@@ -14,6 +16,11 @@ namespace Moon {
 		virtual void UnBind() const = 0;
 
 		virtual const std::string& GetName() const = 0;
+
+		virtual void SetInt(const std::string& name, int value) = 0;
+		virtual void SetFloat3(const std::string& name, const glm::vec3& value) = 0;
+		virtual void SetFloat4(const std::string& name, const glm::vec4& value) = 0;
+		virtual void SetMat4(const std::string& name, const glm::mat4& matrix) = 0;
 
 		static Ref<Shader> Create(const std::string& filepath);
 		static Ref<Shader> Create(const std::string& name, const std::string& vertexSrc, const std::string& fragmentSrc);
