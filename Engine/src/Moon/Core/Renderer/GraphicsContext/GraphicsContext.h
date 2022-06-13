53e3e5f965548b2f7144d89f61d5d08f7c6184ff
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-13 21:14:26 +0200

    Add Create for GraphicsContext

--- Engine/src/Moon/Core/Renderer/GraphicsContext/GraphicsContext.h
+++ Engine/src/Moon/Core/Renderer/GraphicsContext/GraphicsContext.h
@@ -6,8 +6,13 @@ namespace Moon {
 	class GraphicsContext
 	{
 	public:
+		~GraphicsContext() = default;
+
 		virtual void Init() = 0;
 		virtual void SwapBuffers() = 0;
+
+		static Scope<GraphicsContext> Create(void* window);
+
 	};
 
 }
