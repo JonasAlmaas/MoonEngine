b27a62966a31bf46aea1d5ba819db8816cf7815e
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-13 20:30:29 +0200

    Update Base.h

--- Engine/src/Moon/Core/Base.h
+++ Engine/src/Moon/Core/Base.h
@@ -2,27 +2,28 @@
 
 #include <filesystem>
 
+#include "Moon/Core/Config.h"
 #include "Moon/Core/PlatformDetection.h"
 
-
+// ---- Debug ----
 #ifdef ME_DEBUG
 	#if defined(ME_PLATFORM_WINDOWS)
 		#define ME_DEBUGBREAK() __debugbreak()
 	#else
 		#error "Platform doesn't support debugbreak yet!"
 	#endif
-	#define ME_ENABLE_ASSERTS
 #else
 	#define ME_DEBUGBREAK()
+	#define ME_ENABLE_ASSERTS 0
 #endif
+
+// ---- General ----
+#define BIT(x) (1 << x)
 
 #define ME_EXPAND_MACRO(x) x
 #define ME_STRINGIFY_MACRO(x) #x
 
-#define BIT(x) (1 << x)
-
 #define ME_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
-
 
 namespace Moon {
 
