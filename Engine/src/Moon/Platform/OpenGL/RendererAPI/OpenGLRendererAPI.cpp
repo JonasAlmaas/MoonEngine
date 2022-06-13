4ef055b37ca2aa98393ced8dbb54c3a1f1366d06
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-13 17:59:57 +0200

    Enable depth testing for OpenGL

--- Engine/src/Moon/Platform/OpenGL/RendererAPI/OpenGLRendererAPI.cpp
+++ Engine/src/Moon/Platform/OpenGL/RendererAPI/OpenGLRendererAPI.cpp
@@ -10,6 +10,8 @@ namespace Moon {
 	{
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+		glEnable(GL_DEPTH_TEST);
 	}
 
 	void OpenGLRendererAPI::SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height)
@@ -25,7 +27,7 @@ namespace Moon {
 
 	void OpenGLRendererAPI::Clear()
 	{
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	}
 
 	void OpenGLRendererAPI::DrawIndexed(const Ref<VertexArray>& vertexArray)
