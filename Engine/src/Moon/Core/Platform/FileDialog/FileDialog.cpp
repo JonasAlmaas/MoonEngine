bf6882280d496e08f1140c8c94b041e68873672a
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-14 22:16:15 +0200

    Create FileDialog

---
+++ Engine/src/Moon/Core/Platform/FileDialog/FileDialog.cpp
@@ -0,0 +1,16 @@
+#include "mepch.h"
+#include "Moon/Core/Platform/FileDialog/FileDialog.h"
+
+
+namespace Moon {
+
+	Scope<FileDialogAPI> FileDialog::s_FileDialogAPI = nullptr;
+
+	void FileDialog::Init()
+	{
+		ME_PROFILE_FUNCTION();
+
+		s_FileDialogAPI = FileDialogAPI::Create();
+	}
+
+}
---
+++ Engine/src/Moon/Core/Platform/FileDialog/FileDialog.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Moon/Core/Platform/FileDialog/API/FileDialogAPI.h"
+
+
+namespace Moon {
+
+	class FileDialog
+	{
+	public:
+		static void Init();
+
+		/*
+		 * @return Name of the file that was openend.
+		 * Returns an empty string if canceled.
+		 */
+		inline static std::string OpenFile(const char* filter)
+		{
+			return s_FileDialogAPI->OpenFile(filter);
+		}
+
+		/*
+		 * @return Name of the file that was saved.
+		 * Returns an empty string if canceled.
+		 */
+		inline static std::string SaveFile(const char* filter)
+		{
+			return s_FileDialogAPI->SaveFile(filter);
+		}
+
+	private:
+		static Scope<FileDialogAPI> s_FileDialogAPI;
+
+	};
+
+}
