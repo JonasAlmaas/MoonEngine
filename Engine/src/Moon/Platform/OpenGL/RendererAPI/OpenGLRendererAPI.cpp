449bbc47e832d83b72822c6ccd4e644a053bb980
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-15 21:18:12 +0200

    Add DebugMessageCallback to OpenGL

--- Engine/src/Moon/Platform/OpenGL/RendererAPI/OpenGLRendererAPI.cpp
+++ Engine/src/Moon/Platform/OpenGL/RendererAPI/OpenGLRendererAPI.cpp
@@ -6,9 +6,30 @@
 
 namespace Moon {
 
+	void OpenGLMessageCallback(unsigned source, unsigned type, unsigned id, unsigned severity, int length, const char* message, const void* userParam)
+	{
+		switch (severity)
+		{
+			case GL_DEBUG_SEVERITY_HIGH:			ME_CORE_CRITICAL(message); return;
+			case GL_DEBUG_SEVERITY_MEDIUM:			ME_CORE_ERROR(message); return;
+			case GL_DEBUG_SEVERITY_LOW:				ME_CORE_WARN(message); return;
+			case GL_DEBUG_SEVERITY_NOTIFICATION:	ME_CORE_TRACE(message); return;
+		}
+
+		ME_CORE_ASSERT(false, "Unknown severity level!");
+	}
+
 	void OpenGLRendererAPI::Init()
 	{
 		ME_PROFILE_FUNCTION();
+		
+		#ifdef ME_DEBUG
+			glEnable(GL_DEBUG_OUTPUT);
+			glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+			glDebugMessageCallback(OpenGLMessageCallback, nullptr);
+
+			glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_NOTIFICATION, 0, NULL, GL_FALSE);
+		#endif
 
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
