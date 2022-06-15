761c7e14faee30ccd9eb2ca9cea7911c449c4ab7
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-15 21:18:27 +0200

    Change profiling type for renderer functions

--- Engine/src/Moon/Platform/OpenGL/RendererAPI/OpenGLRendererAPI.cpp
+++ Engine/src/Moon/Platform/OpenGL/RendererAPI/OpenGLRendererAPI.cpp
@@ -39,14 +39,14 @@ namespace Moon {
 
 	void OpenGLRendererAPI::SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height)
 	{
-		ME_PROFILE_FUNCTION();
+		ME_PROFILE_RENDERER_FUNCTION();
 
 		glViewport(x, y, width, height);
 	}
 
 	void OpenGLRendererAPI::SetClearColor(const Color& color)
 	{
-		ME_PROFILE_FUNCTION();
+		ME_PROFILE_RENDERER_FUNCTION();
 
 		Color c = color.GetNormalized();
 		glClearColor(c.r, c.g, c.b, c.a);
@@ -54,14 +54,14 @@ namespace Moon {
 
 	void OpenGLRendererAPI::Clear()
 	{
-		ME_PROFILE_FUNCTION();
+		ME_PROFILE_RENDERER_FUNCTION();
 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	}
 
 	void OpenGLRendererAPI::DrawIndexed(const Ref<VertexArray>& vertexArray, uint32_t indexCount)
 	{
-		ME_PROFILE_FUNCTION();
+		ME_PROFILE_RENDERER_FUNCTION();
 
 		uint32_t count = indexCount ? indexCount : vertexArray->GetIndexBuffer()->GetCount();
 		glDrawElements(GL_TRIANGLES, count, GL_UNSIGNED_INT, nullptr);
