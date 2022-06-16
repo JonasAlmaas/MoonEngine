ebe4cda50e968414ead451dcd2ed4493802330ad
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-16 19:07:55 +0200

    Update shader 2D to work with the new batch renderer

--- Sandbox/assets/shaders/2D.glsl
+++ Sandbox/assets/shaders/2D.glsl
@@ -4,18 +4,20 @@
 #version 330 core
 
 layout(location = 0) in vec3 a_Position;
-layout(location = 1) in vec2 a_UV;
+layout(location = 1) in vec4 a_Color;
+layout(location = 2) in vec2 a_UV;
 
+out vec4 v_Color;
 out vec2 v_UV;
 
 uniform mat4 u_ViewProjection;
-uniform mat4 u_Transform;
 
 void main()
 {
+	v_Color = a_Color;
 	v_UV = a_UV;
 
-	gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
+	gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
 }
 
 
@@ -24,13 +26,12 @@ void main()
 
 layout(location = 0) out vec4 o_Color;
 
+in vec4 v_Color;
 in vec2 v_UV;
 
-uniform vec4 u_Color;
-uniform vec2 u_TileFactor;
 uniform sampler2D u_Texture;
 
 void main()
 {
-	o_Color = texture(u_Texture, v_UV * u_TileFactor) * u_Color;
+	o_Color = texture(u_Texture, v_UV) * v_Color;
 }
