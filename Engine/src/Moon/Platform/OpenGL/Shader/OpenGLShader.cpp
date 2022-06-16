4b203baf2b8bae7b958a0a076fbf91be6d41939c
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-16 20:49:10 +0200

    Add SetIntArray to Shader

--- Engine/src/Moon/Platform/OpenGL/Shader/OpenGLShader.cpp
+++ Engine/src/Moon/Platform/OpenGL/Shader/OpenGLShader.cpp
@@ -198,6 +198,13 @@ namespace Moon {
 		UploadUniformInt(name, value);
 	}
 
+	void OpenGLShader::SetIntArray(const std::string& name, int* values, uint32_t count)
+	{
+		ME_PROFILE_FUNCTION();
+
+		UploadUniformIntArray(name, values, count);
+	}
+
 	void OpenGLShader::SetFloat(const std::string& name, float value)
 	{
 		ME_PROFILE_FUNCTION();
@@ -239,6 +246,12 @@ namespace Moon {
 	{
 		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
 		glUniform1i(location, value);
+	}
+
+	void OpenGLShader::UploadUniformIntArray(const std::string& name, int* values, uint32_t count)
+	{
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniform1iv(location, count, values);
 	}
 
 	void OpenGLShader::UploadUniformFloat(const std::string& name, float value)
