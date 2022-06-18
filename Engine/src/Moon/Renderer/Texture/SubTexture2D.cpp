6a6beed5795ac00549a1ef52b910f4f341111f9b
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-18 16:01:44 +0200

    Support non square sub textures

--- Engine/src/Moon/Renderer/Texture/SubTexture2D.cpp
+++ Engine/src/Moon/Renderer/Texture/SubTexture2D.cpp
@@ -4,8 +4,8 @@
 
 namespace Moon {
 
-	SubTexture2D::SubTexture2D(const Ref<Texture2D>& texture, const glm::vec2& min, const glm::vec2& max)
-		: m_Texture(texture)
+	SubTexture2D::SubTexture2D(const Ref<Texture2D>& texture, const glm::vec2& min, const glm::vec2& max, const glm::vec2& spriteSize)
+		: m_Texture(texture), m_SpriteSize(spriteSize)
 	{
 		m_UVCoords[0] = { min.x, min.y };
 		m_UVCoords[1] = { max.x, min.y };
@@ -13,39 +13,39 @@ namespace Moon {
 		m_UVCoords[3] = { min.x, max.y };
 	}
 
-	Ref<SubTexture2D> SubTexture2D::Create(const Ref<Texture2D>& texture, const glm::vec2& spriteSize, const glm::vec2& spriteCoord)
+	Ref<SubTexture2D> SubTexture2D::Create(const Ref<Texture2D>& texture, const glm::vec2& cellSize, const glm::vec2& spriteCoord, const glm::vec2& spriteSize)
 	{
 		ME_PROFILE_FUNCTION();
 
-		float uvFactorX = (1.0f / ((float)texture->GetWidth() / spriteSize.x));
-		float uvFactorY = (1.0f / ((float)texture->GetHeight() / spriteSize.y));
+		float uvFactorX = (1.0f / ((float)texture->GetWidth() / cellSize.x));
+		float uvFactorY = (1.0f / ((float)texture->GetHeight() / cellSize.y));
 
-		glm::vec2 min = { spriteCoord.x * uvFactorX, 1.0f - (spriteCoord.y + 1.0f) * uvFactorY };
-		glm::vec2 max = { (spriteCoord.x + 1.0f) * uvFactorX, 1.0f - (spriteCoord.y) * uvFactorY };
+		glm::vec2 min = { spriteCoord.x * uvFactorX, 1.0f - (spriteCoord.y + spriteSize.y) * uvFactorY };
+		glm::vec2 max = { (spriteCoord.x + spriteSize.x) * uvFactorX, 1.0f - (spriteCoord.y) * uvFactorY };
 
-		return CreateRef<SubTexture2D>(texture, min, max);
+		return CreateRef<SubTexture2D>(texture, min, max, spriteSize);
 	}
 
-	Ref<SubTexture2D> SubTexture2D::Create(const Ref<Texture2D>& texture, const glm::vec2& spriteSize, float spriteIndex)
+	Ref<SubTexture2D> SubTexture2D::Create(const Ref<Texture2D>& texture, const glm::vec2& cellSize, float spriteIndex, const glm::vec2& spriteSize)
 	{
 		ME_PROFILE_FUNCTION();
 
-		float horizontalSpriteCount = (float)texture->GetWidth() / spriteSize.x;
+		float horizontalSpriteCount = (float)texture->GetWidth() / cellSize.x;
 		float a = spriteIndex / horizontalSpriteCount;
 		float x = (a - floor(a)) * horizontalSpriteCount;
 		float y = floor(a);
 
-		return Create(texture, spriteSize, { x, y });
+		return Create(texture, cellSize, { x, y }, spriteSize);
 	}
 
-	Ref<SubTexture2D> SubTexture2D::Create(const Ref<Texture2D>& texture, float spriteSize, float spriteIndex)
+	Ref<SubTexture2D> SubTexture2D::Create(const Ref<Texture2D>& texture, float cellSize, const glm::vec2& spriteCoord, const glm::vec2& spriteSize)
 	{
-		return Create(texture, { spriteSize , spriteSize }, spriteIndex);
+		return Create(texture, { cellSize, cellSize }, spriteCoord, spriteSize);
 	}
 
-	Ref<SubTexture2D> SubTexture2D::Create(const Ref<Texture2D>& texture, float spriteSize, const glm::vec2& spriteCoord)
+	Ref<SubTexture2D> SubTexture2D::Create(const Ref<Texture2D>& texture, float cellSize, float spriteIndex, const glm::vec2& spriteSize)
 	{
-		return Create(texture, { spriteSize , spriteSize }, spriteCoord);
+		return Create(texture, { cellSize, cellSize }, spriteIndex, spriteSize);
 	}
 
 }
--- Engine/src/Moon/Renderer/Texture/SubTexture2D.h
+++ Engine/src/Moon/Renderer/Texture/SubTexture2D.h
@@ -10,50 +10,56 @@ namespace Moon {
 	class SubTexture2D
 	{
 	public:
-		SubTexture2D(const Ref<Texture2D>& texture, const glm::vec2& min, const glm::vec2& max);
+		SubTexture2D(const Ref<Texture2D>& texture, const glm::vec2& min, const glm::vec2& max, const glm::vec2& spriteSize = { 1.0f, 1.0f });
 
 		const Ref<Texture2D> GetTexture() const { return m_Texture; }
 		const glm::vec2* GetUVCoords() const { return m_UVCoords; }
+		const glm::vec2 GetSpriteSize() const { return m_SpriteSize; }
 
 		/**
 		 * Creates a subtexture from a texture. Used for example for textures sheets.
 		 *
 		 * @param texture - Just a Ref<Texture> to your texture sheet.
-		 * @param spriteSize - glm::vec2 describing width and height of the sprite. Unit is pixles.
+		 * @param cellSize - glm::vec2 describing width and height of the sprite. Unit is pixles.
 		 * @param spriteCoord - glm::vec2 describing the position of the sprite on the sheet. { 0, 0 } is top left.
+		 * @param spriteSize - glm::vec2 describing how many cells this sprites takes. { 1, 2 } would be one cell horizonatl and two vertical.
 		 */
-		static Ref<SubTexture2D> Create(const Ref<Texture2D>& texture, const glm::vec2& spriteSize, const glm::vec2& spriteCoord);
+		static Ref<SubTexture2D> Create(const Ref<Texture2D>& texture, const glm::vec2& cellSize, const glm::vec2& spriteCoord, const glm::vec2& spriteSize = { 1.0f, 1.0f });
 
 		/**
 		 * Creates a subtexture from a texture. Used for example for textures sheets.
 		 *
 		 * @param texture - Just a Ref<Texture> to your texture sheet.
-		 * @param spriteSize - glm::vec2 describing width and height of the sprite. Unit is pixles.
-		 * @param Sprite index - This is a float just in case you want to do use it in a silly way.
+		 * @param cellSize - glm::vec2 describing width and height of the sprite. Unit is pixles.
+		 * @param spriteIndex - This is a float just in case you want to do use it in a silly way.
+		 * @param spriteSize - glm::vec2 describing how many cells this sprites takes. { 1, 2 } would be one cell horizonatl and two vertical.
 		 */
-		static Ref<SubTexture2D> Create(const Ref<Texture2D>& texture, const glm::vec2& spriteSize, float spriteIndex);
-
-		/**
-		 * Creates a subtexture from a texture. Used for example for textures sheets.
-		 * 
-		 * @param Texture - Just a Ref<Texture>
-		 * @param Sprite size - One single float describing the size of the sprite. Unit is pixles.
-		 * @param Sprite index - This is a float just in case you want to do use it in a silly way.
-		 */
-		static Ref<SubTexture2D> Create(const Ref<Texture2D>& texture, float spriteSize, float spriteIndex);
+		static Ref<SubTexture2D> Create(const Ref<Texture2D>& texture, const glm::vec2& cellSize, float spriteIndex, const glm::vec2& spriteSize = { 1.0f, 1.0f });
 
 		/**
 		 * Creates a subtexture from a texture. Used for example for textures sheets.
 		 *
 		 * @param texture - Just a Ref<Texture> to your texture sheet.
-		 * @param Sprite size - One single float describing the size of the sprite. Unit is pixles.
+		 * @param cellSize - One single float describing the size of a single cell. Unit is pixles.'
 		 * @param spriteCoord - glm::vec2 describing the position of the sprite on the sheet. { 0, 0 } is top left.
+		 * @param spriteSize - glm::vec2 describing how many cells this sprites takes. { 1, 2 } would be one cell horizonatl and two vertical.
 		 */
-		static Ref<SubTexture2D> Create(const Ref<Texture2D>& texture, float spriteSize, const glm::vec2& spriteCoord);
+		static Ref<SubTexture2D> Create(const Ref<Texture2D>& texture, float cellSize, const glm::vec2& spriteCoord, const glm::vec2& spriteSize = { 1.0f, 1.0f });
+
+		/**
+		 * Creates a subtexture from a texture. Used for example for textures sheets.
+		 *
+		 * @param texture - Just a Ref<Texture> to your texture sheet.
+		 * @param cellSize - One single float describing the size of a single cell. Unit is pixles.
+		 * @param spriteIndex - This is a float just in case you want to do use it in a silly way.
+		 * @param spriteSize - glm::vec2 describing how many cells this sprites takes. { 1, 2 } would be one cell horizonatl and two vertical.
+		 */
+		static Ref<SubTexture2D> Create(const Ref<Texture2D>& texture, float cellSize, float spriteIndex, const glm::vec2& spriteSize = { 1.0f, 1.0f });
 
 	private:
 		Ref<Texture2D> m_Texture;
 		glm::vec2 m_UVCoords[4];
+		glm::vec2 m_SpriteSize;
 
 	};
 
