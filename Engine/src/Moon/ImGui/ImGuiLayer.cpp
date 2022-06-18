0697e184d837ae7f7fae45b6548debef09fc1919
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-18 23:10:49 +0200

    Remove Event handling from ImGuiLayer

--- Engine/src/Moon/ImGui/ImGuiLayer.cpp
+++ Engine/src/Moon/ImGui/ImGuiLayer.cpp
@@ -59,13 +59,6 @@ namespace Moon {
 		ImGui::DestroyContext();
 	}
 
-	void ImGuiLayer::OnEvent(Event& e)
-	{
-		ImGuiIO& io = ImGui::GetIO();
-		e.Handled |= e.IsInCategory(EventCategoryMouse) & io.WantCaptureMouse;
-		e.Handled |= e.IsInCategory(EventCategoryKeyboard) & io.WantCaptureKeyboard;
-	}
-
 	void ImGuiLayer::Begin()
 	{
 		ME_PROFILE_FUNCTION();
--- Engine/src/Moon/ImGui/ImGuiLayer.h
+++ Engine/src/Moon/ImGui/ImGuiLayer.h
@@ -16,7 +16,6 @@ namespace Moon {
 
 		virtual void OnAttach() override;
 		virtual void OnDetach() override;
-		virtual void OnEvent(Event& e) override;
 
 		void Begin();
 		void End();
