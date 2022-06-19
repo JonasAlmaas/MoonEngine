cef5e5d17eb9077226f7da0b11adc504ee7e2c28
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-19 23:52:31 +0200

    Update native scripts from scene

--- Engine/src/Moon/Scene/Scene.cpp
+++ Engine/src/Moon/Scene/Scene.cpp
@@ -27,28 +27,51 @@ namespace Moon {
 		return entity;
 	}
 
+#include "Moon/Scene/Component/NativeScriptComponent.h"
+
 	void Scene::OnUpdate(Timestep ts)
 	{
-		if (m_ActiveCamera == nullptr)
-			return;
-
-		Renderer2D::ResetStats();
-		RenderCommand::SetClearColor({ 0.2f, 0.2f, 0.2f, 1.0f });
-		RenderCommand::Clear();
-
-		auto& camera = m_ActiveCamera->GetComponent<CameraComponent>();
-		auto& cameraTransform = m_ActiveCamera->GetComponent<TransformComponent>();
-		Renderer2D::BeginScene(camera, cameraTransform);
-
-		auto group = m_Registry.group<TransformComponent>(entt::get<SpriteRendererComponent>);
-		for (auto entity : group)
+		// ---- Scripts ----
 		{
-			auto [transform, sprite] = group.get<TransformComponent, SpriteRendererComponent>(entity);
+			m_Registry.view<NativeScriptComponent>().each([=](auto entity, auto& nsc)
+			{
+				if (!nsc.Instance)
+				{
+					nsc.InstantiateFn();
+					nsc.Instance->m_Entity = Entity(&m_Registry, entity);
 
-			Renderer2D::DrawSprite(transform, sprite);
+					if (nsc.OnCreateFn)
+						nsc.OnCreateFn(nsc.Instance);
+				}
+
+				if (nsc.OnUpdateFn)
+					nsc.OnUpdateFn(nsc.Instance, ts);
+			});
 		}
 
-		Renderer2D::EndScene();
+		// ---- Renderer2D ----
+		{
+			if (m_ActiveCamera == nullptr)
+				return;
+
+			Renderer2D::ResetStats();
+			RenderCommand::SetClearColor({ 0.2f, 0.2f, 0.2f, 1.0f });
+			RenderCommand::Clear();
+
+			auto& camera = m_ActiveCamera->GetComponent<CameraComponent>();
+			auto& cameraTransform = m_ActiveCamera->GetComponent<TransformComponent>();
+			Renderer2D::BeginScene(camera, cameraTransform);
+
+			auto group = m_Registry.group<TransformComponent>(entt::get<SpriteRendererComponent>);
+			for (auto entity : group)
+			{
+				auto [transform, sprite] = group.get<TransformComponent, SpriteRendererComponent>(entity);
+
+				Renderer2D::DrawSprite(transform, sprite);
+			}
+
+			Renderer2D::EndScene();
+		}
 	}
 
 	void Scene::OnViewportResize(uint32_t width, uint32_t height)
