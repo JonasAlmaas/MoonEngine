cdbf2d0a61894abee15a9a5b41035b7bb2ce3ffe
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-19 16:45:14 +0200

    Create TransformComponent

---
+++ Engine/src/Moon/Scene/Component/Transform/TransformComponent.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+
+namespace Moon {
+
+	struct TransformComponent
+	{
+		glm::mat4 Transform{ 1.0f };
+
+		TransformComponent() = default;
+		TransformComponent(const TransformComponent&) = default;
+		TransformComponent(const glm::mat4& transform)
+			: Transform(transform) {}
+
+		operator glm::mat4& () { return Transform; }
+		operator const glm::mat4& () const { return Transform; }
+	};
+
+}
