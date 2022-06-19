0fdf5c57ff6ba7689143efed923be79d5577e009
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-20 00:13:38 +0200

    Fix in OpenGL shader reading

--- Engine/src/Moon/Platform/OpenGL/Shader/OpenGLShader.cpp
+++ Engine/src/Moon/Platform/OpenGL/Shader/OpenGLShader.cpp
@@ -59,14 +59,13 @@ namespace Moon {
 
 		std::string result;
 
-		std::ifstream in(filepath, std::ios::in | std::ios::binary);
+		std::ifstream in(filepath, std::ios::in | std::ios::binary); // ifstream closes itself due to RAII
 		if (in)
 		{
 			in.seekg(0, std::ios::end);
 			result.resize(in.tellg());
 			in.seekg(0, std::ios::beg);
 			in.read(&result[0], result.size());
-			in.close();
 		}
 		else {
 			ME_CORE_ERROR("Could not open file '{0}'", filepath);
