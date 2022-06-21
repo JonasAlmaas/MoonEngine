bfee5faf5efe4c865f40a3e63a67d44b23b7a529
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-21 22:06:43 +0200

    Creat PropertiesPanel

---
+++ Editor/src/Asteroid/Panel/Properties/PropertiesPanel.cpp
@@ -0,0 +1,26 @@
+#include "aopch.h"
+#include "Asteroid/Panel/Properties/PropertiesPanel.h"
+
+
+namespace Asteroid {
+	
+	void PropertiesPanel::OnAttach()
+	{
+	}
+
+	void PropertiesPanel::OnDetach()
+	{
+	}
+
+	void PropertiesPanel::OnUpdate(Timestep ts)
+	{
+	}
+
+	void PropertiesPanel::OnImGuiRender()
+	{
+		ImGui::Begin("Properties");
+
+		ImGui::End();
+	}
+
+}
