4cbff9a435b42e9427f782df3c9d54bdbf15c19f
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-22 22:15:14 +0200

    Update Transform component to be easier to work with

--- Engine/src/Moon/Scene/Component/TransformComponent.h
+++ Engine/src/Moon/Scene/Component/TransformComponent.h
@@ -5,15 +5,18 @@ namespace Moon {
 
 	struct TransformComponent
 	{
-		glm::mat4 Transform{ 1.0f };
+		glm::vec3 Translation = { 0.0f, 0.0f, 0.0f };
+		glm::vec3 Rotation = { 0.0f, 0.0f, 0.0f };
+		glm::vec3 Scale = { 1.0f, 1.0f, 1.0f };
 
 		TransformComponent() = default;
 		TransformComponent(const TransformComponent&) = default;
-		TransformComponent(const glm::mat4& transform)
-			: Transform(transform) {}
+		TransformComponent(const glm::vec3& translation)
+			: Translation(translation) {}
 
-		operator glm::mat4& () { return Transform; }
-		operator const glm::mat4& () const { return Transform; }
+		glm::mat4 GetTransform() const { return glm::translate(glm::mat4(1.0f), Translation) * glm::toMat4(glm::quat(Rotation)) * glm::scale(glm::mat4(1.0f), Scale); }
+
+		operator const glm::mat4& () const { return GetTransform(); }
 	};
 
 }
