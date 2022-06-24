179b7757f2b04f094ef0067fb3a0ccac65b564d1
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-24 23:56:38 +0200

    Make sure to actually use the editorconfig

--- premake5.lua
+++ premake5.lua
@@ -1,14 +1,25 @@
+include "./vendor/premake/premake_customization/solution_items.lua"
 include "Dependencies.lua"
 
 workspace "MoonEngine"
     architecture "x86_64"
     startproject "Editor"
 
-    configurations 
+    configurations
 	{
 		"Debug",
 		"Release",
 		"Dist",
+	}
+
+    solution_items
+	{
+		".editorconfig"
+	}
+
+	flags
+	{
+		"MultiProcessorCompile"
 	}
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
