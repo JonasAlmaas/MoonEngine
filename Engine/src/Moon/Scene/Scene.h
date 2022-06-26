7bc991dbe257d027433ca8dd4f0a6d25614c5c00
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-26 20:58:22 +0200

    Update Scene.h

--- Engine/src/Moon/Scene/Scene.h
+++ Engine/src/Moon/Scene/Scene.h
@@ -15,17 +15,21 @@ namespace Moon {
 
 		void OnViewportResize(uint32_t width, uint32_t height);
 
-		void SetActiveCamera(Entity camera);
+		void SetActiveCamera(Entity camera = {});
 		Entity GetActiveCamera() { return m_ActiveCamera; }
 
 		Entity CreateEntity(const std::string& name = std::string());
 		void DestroyEntity(Entity entity);
+
+		Entity GetSelectionContext() { return m_SelectionContext; }
+		void SetSelectionContext(Entity entity = {}) { m_SelectionContext = entity; }
 
 		entt::registry& GetRegistry() { return m_Registry; }
 
 	private:
 		entt::registry m_Registry;
 		Entity m_ActiveCamera{};
+		Entity m_SelectionContext{};
 
 		uint32_t m_ViewportWidth = 0;
 		uint32_t m_ViewportHeight = 0;
