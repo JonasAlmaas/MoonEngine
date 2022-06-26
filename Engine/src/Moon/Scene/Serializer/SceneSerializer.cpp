fe8937b52b2555479be0541075843d11aefb27e0
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-26 19:21:27 +0200

    Add basic scene serializaton

---
+++ Engine/src/Moon/Scene/Serializer/SceneSerializer.cpp
@@ -0,0 +1,299 @@
+#include "mepch.h"
+#include "Moon/Scene/Serializer/SceneSerializer.h"
+
+#include "Moon/Scene/Component/SceneComponents.h"
+#include "Moon/Scene/Entity/Entity.h"
+
+#include <yaml-cpp/yaml.h>
+
+
+namespace YAML {
+
+	template<>
+	struct convert<glm::vec3>
+	{
+		static Node encode(const glm::vec3& rhs)
+		{
+			Node node;
+			node.push_back(rhs.x);
+			node.push_back(rhs.y);
+			node.push_back(rhs.z);
+			return node;
+		}
+
+		static bool decode(const Node& node, glm::vec3& rhs)
+		{
+			if (!node.IsSequence() || node.size() != 3)
+				return false;
+
+			rhs.x = node[0].as<float>();
+			rhs.y = node[1].as<float>();
+			rhs.z = node[2].as<float>();
+			return true;
+		}
+	};
+
+	template<>
+	struct convert<glm::vec4>
+	{
+		static Node encode(const glm::vec4& rhs)
+		{
+			Node node;
+			node.push_back(rhs.x);
+			node.push_back(rhs.y);
+			node.push_back(rhs.z);
+			node.push_back(rhs.w);
+			return node;
+		}
+
+		static bool decode(const Node& node, glm::vec4& rhs)
+		{
+			if (!node.IsSequence() || node.size() != 4)
+				return false;
+
+			rhs.x = node[0].as<float>();
+			rhs.y = node[1].as<float>();
+			rhs.z = node[2].as<float>();
+			rhs.w = node[3].as<float>();
+			return true;
+		}
+	};
+
+	template<>
+	struct convert<Moon::Color>
+	{
+		static Node encode(const Moon::Color& rhs)
+		{
+			Node node;
+			node.push_back(rhs.r);
+			node.push_back(rhs.g);
+			node.push_back(rhs.b);
+			node.push_back(rhs.a);
+			return node;
+		}
+
+		static bool decode(const Node& node, Moon::Color& rhs)
+		{
+			if (!node.IsSequence() || node.size() != 4)
+				return false;
+
+			rhs.r = node[0].as<float>();
+			rhs.g = node[1].as<float>();
+			rhs.b = node[2].as<float>();
+			rhs.a = node[3].as<float>();
+			return true;
+		}
+	};
+
+}
+
+
+namespace Moon {
+
+	YAML::Emitter& operator<<(YAML::Emitter& out, const glm::vec3& v)
+	{
+		out << YAML::Flow;
+		out << YAML::BeginSeq << v.x << v.y << v.z << YAML::EndSeq;
+		return out;
+	}
+
+	YAML::Emitter& operator<<(YAML::Emitter& out, const glm::vec4& v)
+	{
+		out << YAML::Flow;
+		out << YAML::BeginSeq << v.x << v.y << v.z << v.w << YAML::EndSeq;
+		return out;
+	}
+
+	YAML::Emitter& operator<<(YAML::Emitter& out, const Color& v)
+	{
+		// Always serialize colors as normalized
+		Color c = v.GetNormalized();
+
+		out << YAML::Flow;
+		out << YAML::BeginSeq << c.r << c.g << c.b << c.a << YAML::EndSeq;
+		return out;
+	}
+
+	static void SerializeEntity(YAML::Emitter& out, Entity entity)
+	{
+		out << YAML::BeginMap; // Entity
+		out << YAML::Key << "Entity" << YAML::Value << "2783662"; // TODO: Entity ID goes here
+
+		// -- TagComponent --
+		if (entity.HasComponent<TagComponent>())
+		{
+			out << YAML::Key << "TagComponent";
+			out << YAML::BeginMap; // TagComponent
+
+			auto& tag = entity.GetComponent<TagComponent>().Tag;
+			out << YAML::Key << "Tag" << YAML::Value << tag;
+
+			out << YAML::EndMap; // TagComponent
+		}
+
+		// -- TransformComponent --
+		if (entity.HasComponent<TransformComponent>())
+		{
+			out << YAML::Key << "TransformComponent";
+			out << YAML::BeginMap; // TransformComponent
+
+			auto& tc = entity.GetComponent<TransformComponent>();
+
+			out << YAML::Key << "Translation" << YAML::Value << tc.Translation;
+			out << YAML::Key << "Rotation" << YAML::Value << tc.Rotation;
+			out << YAML::Key << "Scale" << YAML::Value << tc.Scale;
+
+			out << YAML::EndMap; // TransformComponent
+		}
+
+		// -- CameraComponent --
+		if (entity.HasComponent<CameraComponent>())
+		{
+			out << YAML::Key << "CameraComponent";
+			out << YAML::BeginMap; // CameraComponent
+
+			auto& cameraComponent = entity.GetComponent<CameraComponent>();
+			auto& camera = cameraComponent.Camera;
+
+			out << YAML::Key << "Camera" << YAML::Value;
+			out << YAML::BeginMap; // Camera
+			out << YAML::Key << "ProjectionType" << YAML::Value << (int)camera.GetProjectionType();
+			out << YAML::Key << "PerspectiveFOV" << YAML::Value << camera.GetPerspectiveVerticalFOV();
+			out << YAML::Key << "PerspectiveNear" << YAML::Value << camera.GetPerspectiveNearClip();
+			out << YAML::Key << "PerspectiveFar" << YAML::Value << camera.GetPerspectiveFarClip();
+			out << YAML::Key << "OrthographicSize" << YAML::Value << camera.GetOrthographicSize();
+			out << YAML::Key << "OrthographicNear" << YAML::Value << camera.GetOrthographicNearClip();
+			out << YAML::Key << "OrthographicFar" << YAML::Value << camera.GetOrthographicFarClip();
+			out << YAML::EndMap; // Camera
+
+			out << YAML::Key << "FixedAspectRatio" << YAML::Value << cameraComponent.FixedAspectRatio;
+
+			out << YAML::EndMap; // CameraComponent
+		}
+
+		// -- SpriteRendererComponent --
+		if (entity.HasComponent<SpriteRendererComponent>())
+		{
+			out << YAML::Key << "SpriteRendererComponent";
+			out << YAML::BeginMap; // SpriteRendererComponent
+
+			auto& spriteRendererComponent = entity.GetComponent<SpriteRendererComponent>();
+			out << YAML::Key << "Color" << YAML::Value << spriteRendererComponent.Color;
+
+			out << YAML::EndMap; // SpriteRendererComponent
+		}
+
+		out << YAML::EndMap; // Entity
+	}
+
+	SceneSerializer::SceneSerializer(const Ref<Scene>& scene)
+		: m_Scene(scene)
+	{
+	}
+
+	void SceneSerializer::Serialize(const std::string& filepath)
+	{
+		YAML::Emitter out;
+		out << YAML::BeginMap;
+		out << YAML::Key << "Scene" << YAML::Value << "Untitled";
+		out << YAML::Key << "ActiveCamera" << YAML::Value << "124356"; // TODO: UUID OF THE ACTIVE CAMERA!
+		out << YAML::Key << "Entities" << YAML::Value << YAML::BeginSeq;
+		m_Scene->GetRegistry().each([&](auto entityID)
+		{
+			Entity entity = { &m_Scene->GetRegistry(), entityID };
+			if (!entity)
+				return;
+
+			SerializeEntity(out, entity);
+		});
+		out << YAML::EndSeq;
+		out << YAML::EndMap;
+
+		std::ofstream fout(filepath);
+		fout << out.c_str();
+	}
+
+	void SceneSerializer::SerializeRuntime(const std::string& filepath)
+	{
+		ME_CORE_ASSERT(false, "SceneSerializer::SerializeRuntime : Not implemented!");
+	}
+
+	bool SceneSerializer::Deserialize(const std::string& filepath)
+	{
+		std::ifstream stream(filepath);
+		std::stringstream strStream;
+		strStream << stream.rdbuf();
+
+		YAML::Node data = YAML::Load(strStream.str());
+		if (!data["Scene"])
+			return false;
+
+		std::string sceneName = data["Scene"].as<std::string>();
+		ME_CORE_TRACE("Deserializing scene '{0}'", sceneName);
+
+		auto entities = data["Entities"];
+		if (entities)
+		{
+			for (auto entity : entities)
+			{
+				uint64_t uuid = entity["Entity"].as<uint64_t>(); // TODO
+
+				std::string name;
+				auto tagComponent = entity["TagComponent"];
+				if (tagComponent)
+					name = tagComponent["Tag"].as<std::string>();
+
+				ME_CORE_TRACE("Deserialized entity with ID = {0}, name = {1}", uuid, name);
+
+				Entity deserializedEntity = m_Scene->CreateEntity(name);
+
+				auto transformComponent = entity["TransformComponent"];
+				if (transformComponent)
+				{
+					// Entities always have transforms
+					auto& tc = deserializedEntity.GetComponent<TransformComponent>();
+					tc.Translation = transformComponent["Translation"].as<glm::vec3>();
+					tc.Rotation = transformComponent["Rotation"].as<glm::vec3>();
+					tc.Scale = transformComponent["Scale"].as<glm::vec3>();
+				}
+
+				auto cameraComponent = entity["CameraComponent"];
+				if (cameraComponent)
+				{
+					auto& cc = deserializedEntity.AddComponent<CameraComponent>();
+
+					cc.Camera.SetProjectionType((SceneCamera::ProjectionType)cameraComponent["Camera"]["ProjectionType"].as<int>());
+
+					cc.Camera.SetPerspectiveVerticalFOV(cameraComponent["Camera"]["PerspectiveFOV"].as<float>());
+					cc.Camera.SetPerspectiveNearClip(cameraComponent["Camera"]["PerspectiveNear"].as<float>());
+					cc.Camera.SetPerspectiveFarClip(cameraComponent["Camera"]["PerspectiveFar"].as<float>());
+
+					cc.Camera.SetOrthographicSize(cameraComponent["Camera"]["OrthographicSize"].as<float>());
+					cc.Camera.SetOrthographicNearClip(cameraComponent["Camera"]["OrthographicNear"].as<float>());
+					cc.Camera.SetOrthographicFarClip(cameraComponent["Camera"]["OrthographicFar"].as<float>());
+
+					cc.Camera.SetOrthographicSize(cameraComponent["Camera"]["OrthographicSize"].as<float>());
+					cc.Camera.SetOrthographicNearClip(cameraComponent["Camera"]["OrthographicNear"].as<float>());
+					cc.Camera.SetOrthographicFarClip(cameraComponent["Camera"]["OrthographicFar"].as<float>());
+				}
+
+				auto spriteRendererComponent = entity["SpriteRendererComponent"];
+				if (spriteRendererComponent)
+				{
+					auto& src = deserializedEntity.AddComponent<SpriteRendererComponent>();
+					auto color = spriteRendererComponent["Color"].as<Color>();
+					src.Color = spriteRendererComponent["Color"].as<Color>();
+				}
+			}
+		}
+
+		return true;
+	}
+
+	bool SceneSerializer::DeserializeRuntime(const std::string& filepath)
+	{
+		ME_CORE_ASSERT(false, "SceneSerializer::DeserializeRuntime : Not implemented!");
+		return false;
+	}
+
+}
---
+++ Engine/src/Moon/Scene/Serializer/SceneSerializer.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Moon/Scene/Scene.h"
+
+
+namespace Moon {
+
+	class SceneSerializer
+	{
+	public:
+		SceneSerializer(const Ref<Scene>& scene);
+
+		void Serialize(const std::string& filepath);
+		void SerializeRuntime(const std::string& filepath);
+
+		bool Deserialize(const std::string& filepath);
+		bool DeserializeRuntime(const std::string& filepath);
+
+	private:
+		Ref<Scene> m_Scene;
+
+	};
+
+}
