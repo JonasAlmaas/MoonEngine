f27cf0f7af83be6ffb96e81c9e764745634e5331
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-30 18:56:16 +0200

    Add vulkan dirs to Dependencies

--- Dependencies.lua
+++ Dependencies.lua
@@ -1,4 +1,7 @@
 -- Moon Engine Dependencies
+
+VULKAN_SDK = os.getenv("VULKAN_SDK")
+
 IncludeDir = {}
 IncludeDir["entt"] = "%{wks.location}/Engine/vendor/entt/include"
 IncludeDir["Glad"] = "%{wks.location}/Engine/vendor/Glad/include"
@@ -6,6 +9,28 @@ IncludeDir["GLFW"] = "%{wks.location}/Engine/vendor/GLFW/include"
 IncludeDir["glm"] = "%{wks.location}/Engine/vendor/glm"
 IncludeDir["ImGui"] = "%{wks.location}/Engine/vendor/ImGui"
 IncludeDir["ImGuizmo"] = "%{wks.location}/Engine/vendor/ImGuizmo"
+IncludeDir["shaderc"] = "%{wks.location}/Engine/vendor/shaderc/include"
 IncludeDir["spdlog"] = "%{wks.location}/Engine/vendor/spdlog/include"
+IncludeDir["SPIRV_Cross"] = "%{wks.location}/Engine/vendor/SPIRV-Cross"
 IncludeDir["stb_image"] = "%{wks.location}/Engine/vendor/stb_image"
+IncludeDir["VulkanSDK"] = "%{VULKAN_SDK}/Include"
 IncludeDir["yaml"] = "%{wks.location}/Engine/vendor/yaml/include"
+
+LibraryDir = {}
+
+LibraryDir["VulkanSDK"] = "%{VULKAN_SDK}/Lib"
+LibraryDir["VulkanSDK_Debug"] = "%{wks.location}/Engine/vendor/VulkanSDK/Lib"
+LibraryDir["VulkanSDK_DebugDLL"] = "%{wks.location}/Engine/vendor/VulkanSDK/Bin"
+
+Library = {}
+Library["Vulkan"] = "%{LibraryDir.VulkanSDK}/vulkan-1.lib"
+Library["VulkanUtils"] = "%{LibraryDir.VulkanSDK}/VkLayer_utils.lib"
+
+Library["ShaderC_Debug"] = "%{LibraryDir.VulkanSDK_Debug}/shaderc_sharedd.lib"
+Library["SPIRV_Cross_Debug"] = "%{LibraryDir.VulkanSDK_Debug}/spirv-cross-cored.lib"
+Library["SPIRV_Cross_GLSL_Debug"] = "%{LibraryDir.VulkanSDK_Debug}/spirv-cross-glsld.lib"
+Library["SPIRV_Tools_Debug"] = "%{LibraryDir.VulkanSDK_Debug}/SPIRV-Toolsd.lib"
+
+Library["ShaderC_Release"] = "%{LibraryDir.VulkanSDK}/shaderc_shared.lib"
+Library["SPIRV_Cross_Release"] = "%{LibraryDir.VulkanSDK}/spirv-cross-core.lib"
+Library["SPIRV_Cross_GLSL_Release"] = "%{LibraryDir.VulkanSDK}/spirv-cross-glsl.lib"
