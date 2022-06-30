39170df8088094b436e511f91d784a4b06c55a7a
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-30 17:07:15 +0200

    Create UniformBuffer

---
+++ Engine/src/Moon/Core/Renderer/Buffer/UniformBuffer/UniformBuffer.cpp
@@ -0,0 +1,22 @@
+#include "mepch.h"
+#include "Moon/Core/Renderer/Buffer/UniformBuffer/UniformBuffer.h"
+
+#include "Moon/Core/Renderer/Renderer.h"
+#include "Moon/Platform/OpenGL/Buffer/UniformBuffer/OpenGLUniformBuffer"
+
+
+namespace Moon {
+
+	Ref<UniformBuffer> UniformBuffer::Create(uint32_t size, uint32_t binding)
+	{
+		switch (Renderer::GetAPI())
+		{
+			case RendererAPI::API::None:			ME_CORE_ASSERT(false, "RendererAPI::API::None is currently not supported"); return nullptr;
+			case RendererAPI::API::OpenGL:			return CreateRef<OpenGLUniformBuffer>(size, binding);
+		}
+
+		ME_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
+}
