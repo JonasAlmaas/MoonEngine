cbb288f3024eb0a3d7c800a6deb9205e9cf0cf2d
JonasAlmaas <jonasalmaas7@gmail.com>
2022-06-30 18:56:36 +0200

    Update 2D shader to work with SPIR-V

--- Editor/Content/Shaders/2D.glsl
+++ Editor/Content/Shaders/2D.glsl
@@ -1,7 +1,7 @@
 // Basic 2D Shader
 
 #type vertex
-#version 450
+#version 450 core
 
 layout(location = 0) in vec3 a_Position;
 layout(location = 1) in vec4 a_Color;
@@ -9,18 +9,27 @@ layout(location = 2) in vec2 a_UV;
 layout(location = 3) in float a_TextureIndex;
 layout(location = 4) in int a_EntityID;
 
-out vec4 v_Color;
-out vec2 v_UV;
-out flat float v_TextureIndex;
-out flat int v_EntityID;
+layout(std140, binding = 0) uniform Camera
+{
+	mat4 u_ViewProjection;
+};
 
-uniform mat4 u_ViewProjection;
+struct VertexOutput
+{
+	vec4 Color;
+	vec2 UV;
+	float TextureIndex;
+};
+
+layout (location = 0) out VertexOutput Output;
+layout (location = 4) out flat int v_EntityID;
 
 void main()
 {
-	v_Color = a_Color;
-	v_UV = a_UV;
-	v_TextureIndex = a_TextureIndex;
+	Output.Color = a_Color;
+	Output.UV = a_UV;
+	Output.TextureIndex = a_TextureIndex;
+
 	v_EntityID = a_EntityID;
 
 	gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
@@ -28,56 +37,61 @@ void main()
 
 
 #type fragment
-#version 450
+#version 450 core
 
 layout(location = 0) out vec4 o_Color;
 layout(location = 1) out int o_EntityID;
 
-in vec4 v_Color;
-in vec2 v_UV;
-in flat float v_TextureIndex;
-in flat int v_EntityID;
+struct VertexOutput
+{
+	vec4 Color;
+	vec2 UV;
+	float TextureIndex;
+};
 
-uniform sampler2D u_Textures[32];
+layout (location = 0) in VertexOutput Input;
+layout (location = 4) in flat int v_EntityID;
+
+layout (binding = 0) uniform sampler2D u_Textures[32];
 
 void main()
 {
-	vec4 color = v_Color;
+	vec4 color = Input.Color;
 
-	switch(int(v_TextureIndex))
+	switch(int(Input.TextureIndex))
 	{
-		case 0:		color *= texture(u_Textures[0], v_UV); break;
-		case 1:		color *= texture(u_Textures[1], v_UV); break;
-		case 2:		color *= texture(u_Textures[2], v_UV); break;
-		case 3:		color *= texture(u_Textures[3], v_UV); break;
-		case 4:		color *= texture(u_Textures[4], v_UV); break;
-		case 5:		color *= texture(u_Textures[5], v_UV); break;
-		case 6:		color *= texture(u_Textures[6], v_UV); break;
-		case 7:		color *= texture(u_Textures[7], v_UV); break;
-		case 8:		color *= texture(u_Textures[8], v_UV); break;
-		case 9:		color *= texture(u_Textures[9], v_UV); break;
-		case 10:	color *= texture(u_Textures[10], v_UV); break;
-		case 11:	color *= texture(u_Textures[11], v_UV); break;
-		case 12:	color *= texture(u_Textures[12], v_UV); break;
-		case 13:	color *= texture(u_Textures[13], v_UV); break;
-		case 14:	color *= texture(u_Textures[14], v_UV); break;
-		case 15:	color *= texture(u_Textures[15], v_UV); break;
-		case 16:	color *= texture(u_Textures[16], v_UV); break;
-		case 17:	color *= texture(u_Textures[17], v_UV); break;
-		case 18:	color *= texture(u_Textures[18], v_UV); break;
-		case 19:	color *= texture(u_Textures[19], v_UV); break;
-		case 20:	color *= texture(u_Textures[20], v_UV); break;
-		case 21:	color *= texture(u_Textures[21], v_UV); break;
-		case 22:	color *= texture(u_Textures[22], v_UV); break;
-		case 23:	color *= texture(u_Textures[23], v_UV); break;
-		case 24:	color *= texture(u_Textures[24], v_UV); break;
-		case 25:	color *= texture(u_Textures[25], v_UV); break;
-		case 26:	color *= texture(u_Textures[26], v_UV); break;
-		case 27:	color *= texture(u_Textures[27], v_UV); break;
-		case 28:	color *= texture(u_Textures[28], v_UV); break;
-		case 29:	color *= texture(u_Textures[29], v_UV); break;
-		case 30:	color *= texture(u_Textures[30], v_UV); break;
-		case 31:	color *= texture(u_Textures[31], v_UV); break;
+		case 0:		color *= texture(u_Textures[0], Input.UV); break;
+		case 1:		color *= texture(u_Textures[1], Input.UV); break;
+		case 2:		color *= texture(u_Textures[2], Input.UV); break;
+		case 3:		color *= texture(u_Textures[3], Input.UV); break;
+		case 4:		color *= texture(u_Textures[4], Input.UV); break;
+		case 5:		color *= texture(u_Textures[5], Input.UV); break;
+		case 6:		color *= texture(u_Textures[6], Input.UV); break;
+		case 7:		color *= texture(u_Textures[7], Input.UV); break;
+		case 8:		color *= texture(u_Textures[8], Input.UV); break;
+		case 9:		color *= texture(u_Textures[9], Input.UV); break;
+		case 10:	color *= texture(u_Textures[10], Input.UV); break;
+		case 11:	color *= texture(u_Textures[11], Input.UV); break;
+		case 12:	color *= texture(u_Textures[12], Input.UV); break;
+		case 13:	color *= texture(u_Textures[13], Input.UV); break;
+		case 14:	color *= texture(u_Textures[14], Input.UV); break;
+		case 15:	color *= texture(u_Textures[15], Input.UV); break;
+		case 16:	color *= texture(u_Textures[16], Input.UV); break;
+		case 17:	color *= texture(u_Textures[17], Input.UV); break;
+		case 18:	color *= texture(u_Textures[18], Input.UV); break;
+		case 19:	color *= texture(u_Textures[19], Input.UV); break;
+		case 20:	color *= texture(u_Textures[20], Input.UV); break;
+		case 21:	color *= texture(u_Textures[21], Input.UV); break;
+		case 22:	color *= texture(u_Textures[22], Input.UV); break;
+		case 23:	color *= texture(u_Textures[23], Input.UV); break;
+		case 24:	color *= texture(u_Textures[24], Input.UV); break;
+		case 25:	color *= texture(u_Textures[25], Input.UV); break;
+		case 26:	color *= texture(u_Textures[26], Input.UV); break;
+		case 27:	color *= texture(u_Textures[27], Input.UV); break;
+		case 28:	color *= texture(u_Textures[28], Input.UV); break;
+		case 29:	color *= texture(u_Textures[29], Input.UV); break;
+		case 30:	color *= texture(u_Textures[30], Input.UV); break;
+		case 31:	color *= texture(u_Textures[31], Input.UV); break;
 	}
 
 	o_Color = color;
