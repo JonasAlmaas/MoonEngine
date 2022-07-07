d7f882a7a00939574247f52df5083d70ebf106fd
JonasAlmaas <jonasalmaas7@gmail.com>
2022-07-07 18:34:20 +0200

    Add new components to SceneComponents.h

--- Engine/src/Moon/Scene/Component/SceneComponents.h
+++ Engine/src/Moon/Scene/Component/SceneComponents.h
@@ -4,4 +4,6 @@
 #include "Moon/Scene/Component/NativeScriptComponent.h"
 #include "Moon/Scene/Component/SpriteRendererComponent.h"
 #include "Moon/Scene/Component/TagComponent.h"
-#include "Moon/Scene/Component/TransformComponent.h"
+#include "Moon/Scene/Component/TransformComponent.h"#include "Moon/Scene/Component/TransformComponent.h"
+#include "Moon/Scene/Component/Physics/BoxCollider2DComponent.h"
+#include "Moon/Scene/Component/Physics/Rigidbody2DComponent.h"
