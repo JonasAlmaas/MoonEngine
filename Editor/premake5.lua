b10f91cc3b1bd6d2fb5489462516775d8f685b25
JonasAlmaas <jonasalmaas7@gmail.com>
2022-07-10 00:23:31 +0200

    Update to use a newer version of Vulkan

--- Editor/premake5.lua
+++ Editor/premake5.lua
@@ -47,11 +47,6 @@ project "Editor"
 		runtime "Debug"
 		symbols "on"
 
-		postbuildcommands
-		{
-			"{COPYDIR} \"%{LibraryDir.VulkanSDK_DebugDLL}\" \"%{cfg.targetdir}\""
-		}
-
 	filter "configurations:Release"
 		defines "ME_RELEASE"
 		runtime "Release"
