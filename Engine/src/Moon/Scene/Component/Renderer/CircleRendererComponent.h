8fadb5f4db6e861e1c71817b24c250aa083cd8ff
JonasAlmaas <jonasalmaas7@gmail.com>
2022-07-09 19:15:50 +0200

    Add comment to CircleRendererComponent

--- Engine/src/Moon/Scene/Component/Renderer/CircleRendererComponent.h
+++ Engine/src/Moon/Scene/Component/Renderer/CircleRendererComponent.h
@@ -7,6 +7,7 @@ namespace Moon {
 
 	struct CircleRendererComponent
 	{
+		// How donuted the circle should be. 1 is not donut at all.
 		float Thickness = 1.0f;
 		float Fade = 0.005f;
 		Moon::Color Color;
