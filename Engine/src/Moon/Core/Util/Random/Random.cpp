fdf48c61b5fd90faeac4a56bb8c294ae569a77ef
JonasAlmaas <jonasalmaas7@gmail.com>
2022-07-10 20:03:59 +0200

    Support more uint types in Random

--- Engine/src/Moon/Core/Util/Random/Random.cpp
+++ Engine/src/Moon/Core/Util/Random/Random.cpp
@@ -5,7 +5,13 @@
 namespace Moon {
 
 	std::mt19937 Random::s_RandomEngine;
-	std::uniform_int_distribution<std::mt19937::result_type> Random::s_Distribution;
+
+	std::uniform_int_distribution<std::mt19937::result_type> Random::s_DistributionFloat;
+
+	std::uniform_int_distribution<uint16_t> Random::s_Distribution16;
+	std::uniform_int_distribution<uint32_t> Random::s_Distribution32;
+	std::uniform_int_distribution<uint64_t> Random::s_Distribution64;
+
 	std::bernoulli_distribution Random::s_BoolDistribution;
 
 	void Random::Init()
@@ -13,19 +19,29 @@ namespace Moon {
 		s_RandomEngine.seed(std::random_device()());
 	}
 
-	uint32_t Random::UInt()
+	uint16_t Random::UInt16()
 	{
-		return s_Distribution(s_RandomEngine);
+		return s_Distribution16(s_RandomEngine);
 	}
 
-	uint32_t Random::UInt(uint32_t min, uint32_t max)
+	uint32_t Random::UInt32()
 	{
-		return min + (s_Distribution(s_RandomEngine) % (max - min + 1));
+		return s_Distribution32(s_RandomEngine);
+	}
+
+	uint64_t Random::UInt()
+	{
+		return s_Distribution64(s_RandomEngine);
+	}
+
+	uint64_t Random::UInt(uint32_t min, uint32_t max)
+	{
+		return min + (s_Distribution64(s_RandomEngine) % (max - min + 1));
 	}
 
 	float Random::Float()
 	{
-		return (float)s_Distribution(s_RandomEngine) / (float)std::numeric_limits<uint32_t>::max();
+		return (float)s_DistributionFloat(s_RandomEngine) / (float)std::numeric_limits<uint32_t>::max();
 	}
 
 	bool Random::Bool()
--- Engine/src/Moon/Core/Util/Random/Random.h
+++ Engine/src/Moon/Core/Util/Random/Random.h
@@ -10,18 +10,28 @@ namespace Moon {
 	public:
 		static void Init();
 
-		/**
-		 * @brief Returns a random unsignend 32 bit integer between min and max. Inclusive.
-		 */
-		static uint32_t UInt();
-		
 		/*
-		 * Returns a random unsignend 32 bit integer between min and max. Inclusive.
+		 * @return A random unsignend 16 bit integer.
+		 */
+		static uint16_t UInt16();
+
+		/*
+		 * @return A random unsignend 32 bit integer.
+		 */
+		static uint32_t UInt32();
+
+		/*
+		 * @return A random unsignend 64 bit integer.
+		 */
+		static uint64_t UInt();
+
+		/*
+		 * Returns a random unsignend 64 bit integer between min and max. Inclusive.
 		 * 
 		 * @param Minimum value the integer can be.
 		 * @param Maximum value the integer can be.
 		 */
-		static uint32_t UInt(uint32_t min, uint32_t max);
+		static uint64_t UInt(uint32_t min, uint32_t max);
 
 		/*
 		 * @brief Returns a random float between 0 and 1. Inclusive.
@@ -35,7 +45,10 @@ namespace Moon {
 
 	private:
 		static std::mt19937 s_RandomEngine;
-		static std::uniform_int_distribution<uint32_t> s_Distribution;
+		static std::uniform_int_distribution<uint16_t> s_Distribution16;
+		static std::uniform_int_distribution<uint32_t> s_Distribution32;
+		static std::uniform_int_distribution<uint64_t> s_Distribution64;
+		static std::uniform_int_distribution<std::mt19937::result_type> s_DistributionFloat;
 		static std::bernoulli_distribution s_BoolDistribution;
 
 	};
