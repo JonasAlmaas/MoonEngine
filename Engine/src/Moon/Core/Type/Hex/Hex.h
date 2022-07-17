48a0d61d5fd1d8f71b572c8b5c5a4f8f1b7c966c
Almaas <jonasalmaas7@gmail.com>
2022-07-17 13:48:46 +0200

    Create Hex class
    Used to convert bytes into hex representation

---
+++ Engine/src/Moon/Core/Type/Hex/Hex.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+namespace Moon {
+
+	class Hex
+	{
+	public:
+		static std::string ByteToHex(uint8_t byte);
+		static uint8_t HexToByte(const std::string& hex);
+
+		static uint8_t DesimalLookup(uint8_t hex);
+		static uint8_t HexLookup(uint8_t desimal);
+
+	};
+
+}
