64a4a25fc9a6cd630c802bd069530160af014904
Almaas <jonasalmaas7@gmail.com>
2022-07-19 18:14:08 +0200

    Add regions to SceneComponents.h

--- Moon-Engine/src/Moon/Scene/Component/SceneComponents.h
+++ Moon-Engine/src/Moon/Scene/Component/SceneComponents.h
@@ -5,8 +5,14 @@
 #include "Moon/Scene/Component/NativeScriptComponent.h"
 #include "Moon/Scene/Component/TagComponent.h"
 #include "Moon/Scene/Component/TransformComponent.h"
+
+#pragma region Physics
 #include "Moon/Scene/Component/Physics/BoxCollider2DComponent.h"
 #include "Moon/Scene/Component/Physics/CircleCollider2DComponent.h"
 #include "Moon/Scene/Component/Physics/Rigidbody2DComponent.h"
+#pragma endregion
+
+#pragma region Renderer
 #include "Moon/Scene/Component/Renderer/CircleRendererComponent.h"
 #include "Moon/Scene/Component/Renderer/SpriteRendererComponent.h"
+#pragma endregion
