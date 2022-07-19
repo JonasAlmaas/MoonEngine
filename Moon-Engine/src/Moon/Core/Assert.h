1620ce5b0c517b1267b4918d8d222a1686c983a9
Almaas <jonasalmaas7@gmail.com>
2022-07-19 17:37:04 +0200

    Fix ME_ASSERT

--- Moon-Engine/src/Moon/Core/Assert.h
+++ Moon-Engine/src/Moon/Core/Assert.h
@@ -4,7 +4,7 @@
 #if ME_ENABLE_ASSERTS
 	// Alteratively we could use the same "default" message for both "WITH_MSG" and "NO_MSG" and
 	// provide support for custom formatting by concatenating the formatting string instead of having the format inside the default message
-	#define ME_INTERNAL_ASSERT_IMPL(type, check, msg, ...) { if(!(check)) { ME##type##ERROR(msg, __VA_ARGS__); ME_DEBUGBREAK(); } }
+	#define ME_INTERNAL_ASSERT_IMPL(type, check, msg, ...) { if(!(check)) { ME##type##LOG_ERROR(msg, __VA_ARGS__); ME_DEBUGBREAK(); } }
 	#define ME_INTERNAL_ASSERT_WITH_MSG(type, check, ...) ME_INTERNAL_ASSERT_IMPL(type, check, "Assertion failed: {0}", __VA_ARGS__)
 	#define ME_INTERNAL_ASSERT_NO_MSG(type, check) ME_INTERNAL_ASSERT_IMPL(type, check, "Assertion '{0}' failed at {1}:{2}", ME_STRINGIFY_MACRO(check), std::filesystem::path(__FILE__).filename().string(), __LINE__)
 
@@ -13,7 +13,7 @@
 
 	// Currently accepts at least the condition and one additional parameter (the message) being optional
 	#define ME_ASSERT(...) ME_EXPAND_MACRO( ME_INTERNAL_ASSERT_GET_MACRO(__VA_ARGS__)(_, __VA_ARGS__) )
-	#define ME_CORE_ASSERT(...) ME_EXPAND_MACRO( ME_INTERNAL_ASSERT_GET_MACRO(__VA_ARGS__)(_CORE_LOG_, __VA_ARGS__) )
+	#define ME_CORE_ASSERT(...) ME_EXPAND_MACRO( ME_INTERNAL_ASSERT_GET_MACRO(__VA_ARGS__)(_CORE_, __VA_ARGS__) )
 #else
 	#define ME_ASSERT(...)
 	#define ME_CORE_ASSERT(...)
