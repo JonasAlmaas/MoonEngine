ca2395ca8d66ffdad9936fc8911130bc2cd58fc3
Jonas Almås <44512227+JonasAlmaas@users.noreply.github.com>
2022-08-06 07:27:50 +0200

    Create noise from Specification struct

--- Moon-Engine/src/private/Moon/Core/Math/Noise.cpp
+++ Moon-Engine/src/private/Moon/Core/Math/Noise.cpp
@@ -6,14 +6,27 @@
 
 namespace Moon {
 
-	Noise::Noise()
+	Noise::Noise(const Specification& spec)
+		: m_Specification(spec)
 	{
-		m_FastNoise = new FastNoise();
-	}
+		m_FastNoise = new FastNoise(m_Specification.Seed);
+		m_FastNoise->SetFrequency(m_Specification.Frequency);
+		m_FastNoise->SetInterp((FastNoise::Interp)m_Specification.InterpMethod);
+		m_FastNoise->SetNoiseType((FastNoise::NoiseType)m_Specification.Type);
+		m_FastNoise->SetFractalType((FastNoise::FractalType)m_Specification.FractalType);
+		m_FastNoise->SetFractalOctaves(m_Specification.FractalOctaves);
+		m_FastNoise->SetFractalLacunarity(m_Specification.FractalLacunarity);
+		m_FastNoise->SetFractalGain(m_Specification.FractalGain);
 
-	Noise::Noise(int seed)
-	{
-		m_FastNoise = new FastNoise(seed);
+		if (m_Specification.NoiseLookup != nullptr)
+		{
+			m_FastNoise->SetCellularNoiseLookup(m_Specification.NoiseLookup->m_FastNoise);
+			m_FastNoise->SetCellularDistanceFunction((FastNoise::CellularDistanceFunction)m_Specification.CellularDistanceFunction);
+			m_FastNoise->SetCellularReturnType((FastNoise::CellularReturnType)m_Specification.CellularReturnType);
+		}
+		
+		m_FastNoise->SetCellularDistance2Indices(m_Specification.CellularDistance2Indices.x, m_Specification.CellularDistance2Indices.y);
+		m_FastNoise->SetCellularJitter(m_Specification.CellularJitter);
 	}
 
 	Noise::~Noise()
@@ -21,7 +34,6 @@ namespace Moon {
 		delete m_FastNoise;
 	}
 
-	// -- Get Noise --
 	float Noise::Get(float x, float y)
 	{
 		return m_FastNoise->GetNoise(x, y);
@@ -30,105 +42,6 @@ namespace Moon {
 	float Noise::Get(float x, float y, float z)
 	{
 		return m_FastNoise->GetNoise(x, y, z);
-	}
-
-	// -- Noise Type --
-	void Noise::SetNoiseType(Type noiseType)
-	{
-		m_FastNoise->SetNoiseType((FastNoise::NoiseType)noiseType);
-	}
-
-	Noise::Type Noise::GetNoiseType()
-	{
-		return (Type)m_FastNoise->GetNoiseType();
-	}
-
-	// -- Interpolation Method --
-	void Noise::SetInterp(InterpMethod method)
-	{
-		m_FastNoise->SetInterp((FastNoise::Interp)method);
-	}
-
-	Noise::InterpMethod Noise::GetInterp()
-	{
-		return (InterpMethod)m_FastNoise->GetInterp();
-	}
-
-	// -- Frequency --
-	void Noise::SetFrequency(float frequency)
-	{
-		m_FastNoise->SetFrequency(frequency);
-	}
-
-	float Noise::GetFrequency() const
-	{
-		return m_FastNoise->GetFrequency();
-	}
-
-	// -- Fractal Octaves --
-	void Noise::SetFractalOctaves(int octaves)
-	{
-		m_FastNoise->SetFractalOctaves(octaves);
-	}
-
-	int Noise::GetFractalOctaves() const
-	{
-		return m_FastNoise->GetFractalOctaves();
-	}
-
-	// -- Fractal Lacunarity --
-	void Noise::SetFractalLacunarity(float lacunarity)
-	{
-		return m_FastNoise->SetFractalLacunarity(lacunarity);
-	}
-
-	float Noise::GetFractalLacunarity() const
-	{
-		return m_FastNoise->GetFractalLacunarity();
-	}
-
-	// -- Fractal Gain --
-	void Noise::SetFractalGain(float gain)
-	{
-		m_FastNoise->SetFractalGain(gain);
-	}
-
-	float Noise::GetFractalGain() const
-	{
-		return m_FastNoise->GetFractalGain();
-	}
-
-	// -- Fractal Type --
-	void Noise::SetFractalType(FractalType fractalType)
-	{
-		m_FastNoise->SetFractalType((FastNoise::FractalType)fractalType);
-	}
-
-	Noise::FractalType Noise::GetFractalType() const
-	{
-		return (FractalType)m_FastNoise->GetFractalType();
-	}
-
-	// ----------------------------
-	// -------- Static API --------
-	// ----------------------------
-
-	static FastNoise s_FastNoise;
-
-	void Noise::SetSeed(int seed)
-	{
-		s_FastNoise.SetSeed(seed);
-	}
-
-	int Noise::GetSeed()
-	{
-		return s_FastNoise.GetSeed();
-	}
-
-	float Noise::PerlinNoise(float x, float y)
-	{
-		s_FastNoise.SetNoiseType(FastNoise::Perlin);
-		return s_FastNoise.GetNoise(x, y);
 	}
 
 }
--- Moon-Engine/src/public/Moon/Core/Math/Noise.h
+++ Moon-Engine/src/public/Moon/Core/Math/Noise.h
@@ -35,9 +35,76 @@ namespace Moon {
 			RigidMulti
 		};
 
+		enum class CellularDistanceFunction
+		{
+			Euclidean,
+			Manhattan,
+			Natural
+		};
+
+		enum class CellularReturnType
+		{
+			CellValue,
+			NoiseLookup,
+			Distance,
+			Distance2,
+			Distance2Add,
+			Distance2Sub,
+			Distance2Mul,
+			Distance2Div
+		};
+
+		struct Specification
+		{
+			int Seed = 1337;
+
+			// Sets frequency for all noise types
+			float Frequency = 0.01f;
+
+			// Changes the interpolation method used to smooth between noise values
+			// Possible interpolation methods (lowest to highest quality) :
+			// - Linear
+			// - Hermite
+			// - Quintic
+			// Used in Value, Perlin Noise and Position Warping
+			InterpMethod InterpMethod = InterpMethod::Quintic;
+
+			// Sets noise return type of Get(...)
+			Type Type = Type::Simplex;
+
+			// Sets method for combining octaves in all fractal noise types
+			FractalType FractalType = FractalType::FBM;
+
+			// Sets octave count for all fractal noise types
+			uint32_t FractalOctaves = 3;
+
+			// Sets octave lacunarity for all fractal noise types
+			float FractalLacunarity = 2.0f;
+			 
+			// Sets octave gain for all fractal noise types
+			float FractalGain = 0.5f;
+
+			// Sets distance function used in cellular noise calculations
+			CellularDistanceFunction CellularDistanceFunction = CellularDistanceFunction::Euclidean;
+
+			// Note: NoiseLookup requires another Noise object be set with SetCellularNoiseLookup() to function
+			CellularReturnType CellularReturnType = CellularReturnType::CellValue;
+
+			// Noise used to calculate a cell value if cellular return type is NoiseLookup
+			Noise* NoiseLookup = nullptr;
+
+			// Sets the 2 distance indices used for distance2 return types
+			// Note: index0 should be lower than index1
+			// Both indices must be >= 0, index1 must be < 4
+			glm::uvec2 CellularDistance2Indices = { 0, 1 };
+
+			// Sets the maximum distance a cellular point can move from its grid position
+			// Setting this high will make artifacts more common
+			float CellularJitter = 0.45f;
+		};
+
 	public:
-		Noise();
-		Noise(int seed);
+		Noise(const Specification& spec = Specification());
 		~Noise();
 
 		/**
@@ -47,69 +114,11 @@ namespace Moon {
 		float Get(float x, float y);
 		float Get(float x, float y, float z);
 
-		/**
-		 * Sets noise return type of GetNoise(...)
-		 * Default: Simplex
-		 */
-		void SetNoiseType(Type type);
-		Type GetNoiseType();
-
-		/**
-		 * Changes the interpolation method used to smooth between noise values
-		 * Possible interpolation methods (lowest to highest quality) :
-		 * - Linear
-		 * - Hermite
-		 * - Quintic
-		 * Used in Value, Perlin Noise and Position Warping
-		 * Default: Quintic
-		 */
-		void SetInterp(InterpMethod method);
-		InterpMethod GetInterp();
-
-		/**
-		 * Sets frequency for all noise types.
-		 * Default: 0.01
-		 */
-		void SetFrequency(float frequency);
-		float GetFrequency() const;
-
-		/**
-		* Sets octave count for all fractal noise types.
-		* Default: 3
-		*/
-		void SetFractalOctaves(int octaves);
-		int GetFractalOctaves() const;
-
-		/**
-		 * Sets octave lacunarity for all fractal noise types.
-		 * Default: 2.0
-		 */
-		void SetFractalLacunarity(float lacunarity);
-		float GetFractalLacunarity() const;
-
-		/**
-		 * Sets octave gain for all fractal noise types.
-		 * Default: 0.5
-		*/
-		void SetFractalGain(float gain);
-		float GetFractalGain() const;
-
-		/**
-		 * Sets method for combining octaves in all fractal noise types.
-		 * Default: FBM
-		*/
-		void SetFractalType(FractalType fractalType);
-		FractalType GetFractalType() const;
-
-	public:
-		// -- Static API --
-		static void SetSeed(int seed);
-		static int GetSeed();
-
-		static float PerlinNoise(float x, float y);
+		const Specification& GetSpecification() const { return m_Specification; }
 
 	private:
 		FastNoise* m_FastNoise = nullptr;
+		Specification m_Specification;
 
 	};
 
