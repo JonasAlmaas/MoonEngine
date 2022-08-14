b5298f6354de040a8de35298ffa1e89514797106
Almaas <jonasalmaas7@gmail.com>
2022-08-14 20:17:53 +0200

    Move shaders into core folder
    This is to separate engine shaders from game shaders

--- Moon-Editor/Content/Shaders/Core/Text.glsl
+++ Moon-Editor/Content/Shaders/Core/Text.glsl
@@ -6,7 +6,6 @@
 layout(location = 0) in vec3 a_Position;
 layout(location = 1) in vec4 a_Color;
 layout(location = 2) in vec2 a_UV;
-layout(location = 3) in float a_TextureIndex;
 
 layout(std140, binding = 0) uniform Camera
 {
@@ -20,14 +19,11 @@ struct VertexOutput
 };
 
 layout (location = 0) out VertexOutput Output;
-layout (location = 3) out flat float v_TextureIndex;
 
 void main()
 {
 	Output.Color = a_Color;
 	Output.UV = a_UV;
-
-	v_TextureIndex = a_TextureIndex;
 
 	gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
 }
@@ -47,7 +43,7 @@ struct VertexOutput
 layout (location = 0) in VertexOutput Input;
 layout (location = 3) in flat float v_TextureIndex;
 
-layout (binding = 0) uniform sampler2D u_FontAtlases[32];
+layout (binding = 0) uniform sampler2D u_FontAtlas;
 
 float median(float r, float g, float b)
 {
@@ -57,7 +53,7 @@ float median(float r, float g, float b)
 float ScreenPxRange()
 {
 	float pxRange = 2.0f;
-    vec2 unitRange = vec2(pxRange) / vec2(textureSize(u_FontAtlases[int(v_TextureIndex)], 0));
+    vec2 unitRange = vec2(pxRange) / vec2(textureSize(u_FontAtlas, 0));
     vec2 screenTexSize = vec2(1.0) / fwidth(Input.UV);
     return max(0.5*dot(unitRange, screenTexSize), 1.0);
 }
@@ -67,7 +63,7 @@ void main()
 	vec4 bgColor = vec4(Input.Color.rgb, 0.0);
 	vec4 fgColor = Input.Color;
 
-	vec3 msd = texture(u_FontAtlases[int(v_TextureIndex)], Input.UV).rgb;
+	vec3 msd = texture(u_FontAtlas, Input.UV).rgb;
     float sd = median(msd.r, msd.g, msd.b);
     float screenPxDistance = ScreenPxRange() * (sd - 0.5);
     float opacity = clamp(screenPxDistance + 0.5, 0.0, 1.0);
