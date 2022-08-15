3d40f1e36b6e8c6837985ffe73e1cf920aa4f652
Almaas <jonasalmaas7@gmail.com>
2022-08-15 10:35:04 +0200

    Update Renderer2D_Sprite.glsl

--- Moon-Editor/Content/Shaders/Core/Renderer2D/Renderer2D_Sprite.glsl
+++ Moon-Editor/Content/Shaders/Core/Renderer2D/Renderer2D_Sprite.glsl
@@ -54,47 +54,20 @@ layout (location = 4) in flat int v_EntityID;
 
 layout (binding = 0) uniform sampler2D u_Textures[32];
 
+float Lerp(float x, float y, float a)
+{
+	return x + ((y - x) * a);
+}
+
+vec3 Lerp3(vec3 v1, vec3 v2, float a)
+{
+	return vec3(Lerp(v1.x, v2.x, a), Lerp(v1.y, v2.y, a), Lerp(v1.z, v2.z, a));
+}
+
 void main()
 {
-	vec4 color = Input.Color;
-
-	switch(int(v_TextureIndex))
-	{
-		case 0:		color *= texture(u_Textures[0], Input.UV); break;
-		case 1:		color *= texture(u_Textures[1], Input.UV); break;
-		case 2:		color *= texture(u_Textures[2], Input.UV); break;
-		case 3:		color *= texture(u_Textures[3], Input.UV); break;
-		case 4:		color *= texture(u_Textures[4], Input.UV); break;
-		case 5:		color *= texture(u_Textures[5], Input.UV); break;
-		case 6:		color *= texture(u_Textures[6], Input.UV); break;
-		case 7:		color *= texture(u_Textures[7], Input.UV); break;
-		case 8:		color *= texture(u_Textures[8], Input.UV); break;
-		case 9:		color *= texture(u_Textures[9], Input.UV); break;
-		case 10:	color *= texture(u_Textures[10], Input.UV); break;
-		case 11:	color *= texture(u_Textures[11], Input.UV); break;
-		case 12:	color *= texture(u_Textures[12], Input.UV); break;
-		case 13:	color *= texture(u_Textures[13], Input.UV); break;
-		case 14:	color *= texture(u_Textures[14], Input.UV); break;
-		case 15:	color *= texture(u_Textures[15], Input.UV); break;
-		case 16:	color *= texture(u_Textures[16], Input.UV); break;
-		case 17:	color *= texture(u_Textures[17], Input.UV); break;
-		case 18:	color *= texture(u_Textures[18], Input.UV); break;
-		case 19:	color *= texture(u_Textures[19], Input.UV); break;
-		case 20:	color *= texture(u_Textures[20], Input.UV); break;
-		case 21:	color *= texture(u_Textures[21], Input.UV); break;
-		case 22:	color *= texture(u_Textures[22], Input.UV); break;
-		case 23:	color *= texture(u_Textures[23], Input.UV); break;
-		case 24:	color *= texture(u_Textures[24], Input.UV); break;
-		case 25:	color *= texture(u_Textures[25], Input.UV); break;
-		case 26:	color *= texture(u_Textures[26], Input.UV); break;
-		case 27:	color *= texture(u_Textures[27], Input.UV); break;
-		case 28:	color *= texture(u_Textures[28], Input.UV); break;
-		case 29:	color *= texture(u_Textures[29], Input.UV); break;
-		case 30:	color *= texture(u_Textures[30], Input.UV); break;
-		case 31:	color *= texture(u_Textures[31], Input.UV); break;
-	}
-
-	o_Color = color;
+	vec4 color = texture(u_Textures[int(v_TextureIndex)], Input.UV);
+	o_Color = vec4(Lerp3(color.xyz, Input.Color.xyz, Input.Color.a), color.a);
 
 	if (o_Color.a == 0.0)
 		discard;
